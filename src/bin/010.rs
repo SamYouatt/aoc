610a32ac32fec44fb4a6f9a406d0cda8d9a4f442
Sam Youatt <sdyouatt@btinternet.com>
2021-08-30 15:31:20 +0100

    ✨ day 10 completed

--- src/bin/010.rs
+++ src/bin/010.rs
@@ -1,4 +1,4 @@
-use std::time::Instant;
+use std::{collections::HashMap, time::Instant};
 
 fn main() {
     let mut numbers: Vec<usize> = include_str!("../../inputs/010.txt")
@@ -7,11 +7,17 @@ fn main() {
         .collect();
     numbers.push(0);
     numbers.sort_unstable();
+    numbers.push(numbers.last().unwrap() + 3);
 
     let start = Instant::now();
     let answer = get_split(&numbers);
 
     println!("Part one: {}", answer.0 * answer.1);
+    println!("Time taken: {:#?}", start.elapsed());
+
+    let start = Instant::now();
+    let answer = find_routes(&numbers);
+    println!("Part two: {}", answer);
     println!("Time taken: {:#?}", start.elapsed());
 }
 
@@ -25,9 +31,32 @@ fn get_split(numbers: &[usize]) -> (usize, usize) {
             num_ones += 1;
         }
     });
-    num_threes += 1;
 
     (num_ones, num_threes)
+}
+
+fn find_routes(numbers: &[usize]) -> usize {
+    //let mut sum = 0;
+
+    let mut paths = HashMap::new();
+
+    paths.insert(numbers.last().copied().unwrap(), 1);
+
+    for i in (0..(numbers.len() - 1)).into_iter().rev() {
+        let neighbours = ((i + 1)..=std::cmp::min(i + 3, numbers.len() - 1))
+            .into_iter()
+            .filter_map(|j| {
+                if (1..=3).contains(&(numbers[j] - numbers[i])) {
+                    Some(paths.get(&numbers[j]).unwrap())
+                } else {
+                    None
+                }
+            })
+            .sum();
+        paths.insert(numbers[i], neighbours);
+    }
+
+    *paths.get(&0).unwrap()
 }
 
 #[test]
@@ -39,8 +68,24 @@ fn test_get_split() {
 
     numbers.push(0);
     numbers.sort_unstable();
+    numbers.push(numbers.last().unwrap() + 3);
 
     let split = get_split(&numbers);
     assert_eq!(split.0, 22);
     assert_eq!(split.1, 10);
+}
+
+#[test]
+fn test_number_routes() {
+    let mut numbers: Vec<usize> = "16\n10\n15\n5\n1\n11\n7\n19\n6\n12\n4"
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .collect();
+
+    numbers.push(0);
+    numbers.sort_unstable();
+    numbers.push(numbers.last().unwrap() + 3);
+
+    let routes = find_routes(&numbers);
+    assert_eq!(routes, 8);
 }
