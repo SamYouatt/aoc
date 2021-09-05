41477bb1b1a3f0dc292a5f678f1cae9951887c60
Sam Youatt <sdyouatt@btinternet.com>
2021-09-05 11:27:50 +0100

    ✨ day 14 part 1 completed

--- src/bin/014.rs
+++ src/bin/014.rs
@@ -1,38 +1,68 @@
+use std::collections::HashMap;
+
+use regex::Regex;
+
+struct Mask {
+    set: usize,
+    clear: usize,
+}
+
 fn main() {
     let input = include_str!("../../inputs/014.txt");
+    let mut memory: HashMap<usize, usize> = HashMap::new();
+
+    // using this regex expression can capture the values in the brackets, the memory address and the value
+    let reg = Regex::new(r#"^mem\[(\d+)\] = (\d+)$"#).unwrap();
 
     // the first number in the tuple will be all the bits that need to be set (stored as int)
     // second number is all the bits that need to be cleared (again stored as int)
-    let mut set_clear = (0, 0);
+    let mut mask = Mask {
+        set: 0,
+        clear: usize::MAX,
+    };
 
     input.lines().for_each(|line| {
         if line.starts_with("ma") {
-            // update the mask
-            set_clear = line
+            mask = line
                 .split(" = ")
+                // ignore the first str in the split
                 .nth(1)
                 .unwrap()
                 .bytes()
+                // reverse so can operate on bits least significant to most significant
                 .rev()
                 .enumerate()
-                .fold((usize::MAX, 0), |(set, clear), (i, byte)| {
-                    println!("i: {}, byte: {}", i, byte);
-                    match byte {
-                        b'1' => (set & !(1 << i), clear),
-                        b'0' => (set, clear | 1 << i),
-                        _ => (set, clear),
-                    }
-                })
+                // starting with default values, if a 0 is encountered at a bit then set the bit in the clear value
+                // if a 1 is encountered then set this bit in the set value
+                // if it isn't its an X so we can ignore it
+                .fold(
+                    Mask {
+                        set: 0,
+                        clear: usize::MAX,
+                    },
+                    |mask, (i, byte)| match byte {
+                        b'0' => Mask {
+                            set: mask.set,
+                            clear: mask.clear & !(1 << i),
+                        },
+                        b'1' => Mask {
+                            set: mask.set | 1 << i,
+                            clear: mask.clear,
+                        },
+                        _ => Mask { ..mask },
+                    },
+                );
         } else {
-            // get the memory address and value
-            // apply the mask to the memory address
-            // set the value at the memory address
+            let instruction = reg.captures(&line).unwrap();
+
+            // the fist element in the capture is the whole string so need the next two
+            memory.insert(
+                instruction[1].parse::<usize>().unwrap(),
+                // anding a 0 always gives 0, oring a 1 always gives 1, so sets the bits accordingly
+                instruction[2].parse::<usize>().unwrap() & mask.clear | mask.set,
+            );
         }
     });
 
-    println!("set: {}, clear: {}", set_clear.0, set_clear.1);
-}
-
-fn apply_mask(mask: &[Option<bool>], memory_location: usize) -> usize {
-    0
+    println!("Part one: {}", memory.values().sum::<usize>());
 }
