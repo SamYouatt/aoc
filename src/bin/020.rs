4bd7611693f319e5fc2afb51b4286338d281e62b
Sam Youatt <sdyouatt@btinternet.com>
2021-09-13 16:04:38 +0100

    ✨ started work on part 2

--- src/bin/020.rs
+++ src/bin/020.rs
@@ -1,12 +1,43 @@
-use std::{collections::HashMap, time::Instant};
+use std::{
+    collections::{HashMap, LinkedList},
+    time::Instant,
+};
 
-struct Tile {
+struct TileEdges {
     id: usize,
     edges: [u16; 4],
 }
 
+#[derive(Debug, Clone)]
+struct Tile {
+    id: usize,
+    grid: Vec<Vec<u8>>,
+}
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+struct Coord {
+    x: isize,
+    y: isize,
+}
+
+impl Coord {
+    fn from(x: isize, y: isize) -> Coord {
+        Coord { x, y }
+    }
+}
+
 fn main() {
-    let tiles = include_str!("../../inputs/020.txt")
+    let input = include_str!("../../inputs/020.txt");
+
+    let start = Instant::now();
+    println!("Part one: {} in {:#?}", part_one(input), start.elapsed());
+
+    let start = Instant::now();
+    println!("Part two: {} in {:#?}", part_two(input), start.elapsed());
+}
+
+fn part_one(input: &str) -> usize {
+    let tiles = input
         .split("\n\n")
         .map(|chunk| {
             let (id, tile) = chunk.split_once('\n').unwrap();
@@ -33,18 +64,13 @@ fn main() {
                     edge << 1 | if pixel == b'#' { 1 } else { 0 }
                 });
 
-            Tile {
+            TileEdges {
                 id,
                 edges: [top_edge, left_edge, right_edge, bottom_edge],
             }
         })
-        .collect::<Vec<Tile>>();
+        .collect::<Vec<TileEdges>>();
 
-    let start = Instant::now();
-    println!("Part one: {} in {:#?}", part_one(&tiles), start.elapsed());
-}
-
-fn part_one(tiles: &[Tile]) -> usize {
     let edge_matches: HashMap<u16, usize> = tiles
         .iter()
         .flat_map(|tile| {
@@ -73,4 +99,121 @@ fn part_one(tiles: &[Tile]) -> usize {
         })
         .map(|tile| tile.id)
         .product::<usize>()
+}
+
+fn part_two(input: &str) -> usize {
+    let mut tiles: Vec<Tile> = input
+        .split("\n\n")
+        .map(|chunk| {
+            let (id, tile) = chunk.split_once('\n').unwrap();
+            let id: usize = id[5..].trim_end_matches(':').parse().unwrap();
+
+            let pixels: Vec<Vec<u8>> = tile
+                .split('\n')
+                .map(|line| line.bytes().collect())
+                .collect();
+
+            Tile { id, grid: pixels }
+        })
+        .collect();
+
+    let mut picture = HashMap::<Coord, Tile>::new();
+    let mut queue = LinkedList::<Coord>::new();
+
+    picture.insert(Coord::from(0, 0), tiles.pop().unwrap());
+    queue.push_back(Coord::from(0, 0));
+
+    while let Some(position) = queue.pop_front() {
+        let mut remove = vec![];
+
+        tiles.iter().enumerate().for_each(|(i, tile)| {
+            let mut pixels = tile.grid.clone();
+            'variations: for _flip in 0..=1 {
+                for _rotation in 0..4 {
+                    // see if it fits and if it does add it to the picture at the appropriate relative coords
+                    if let Some(offset) = find_fit(&picture[&position].grid, &pixels) {
+                        let new_position =
+                            Coord::from(position.x + offset.x, position.y + offset.y);
+
+                        if let std::collections::hash_map::Entry::Vacant(e) =
+                            picture.entry(new_position)
+                        {
+                            remove.push(i);
+                            e.insert(tile.clone());
+                            queue.push_back(new_position);
+                            break 'variations;
+                        }
+                    }
+                    pixels = rotate(pixels);
+                }
+                pixels = flip(pixels);
+            }
+        });
+
+        remove.iter().for_each(|id| {
+            tiles.remove(*id);
+        });
+    }
+
+    println!("{:#?}", picture[&Coord::from(0, 0)]);
+
+    0
+}
+
+fn find_fit(grid1: &Vec<Vec<u8>>, grid2: &Vec<Vec<u8>>) -> Option<Coord> {
+    if grid1.first() == grid2.last() {
+        Some(Coord::from(-1, 0))
+    } else if grid1.last() == grid2.first() {
+        Some(Coord::from(1, 0))
+    } else {
+        let mut on_left = true;
+        let mut on_right = true;
+
+        for x in 0..grid1.len() {
+            on_left &= grid1[x].first() == grid2[x].last();
+            on_right &= grid1[x].last() == grid2[x].first();
+        }
+
+        if on_left {
+            Some(Coord::from(0, -1))
+        } else if on_right {
+            Some(Coord::from(0, 1))
+        } else {
+            None
+        }
+    }
+}
+
+fn flip<T>(grid: Vec<Vec<T>>) -> Vec<Vec<T>>
+where
+    T: Copy,
+{
+    let original_size = (grid.len(), grid[0].len());
+
+    let mut new_grid: Vec<Vec<T>> = Vec::with_capacity(original_size.0);
+    for x in 0..original_size.0 {
+        new_grid.push(Vec::with_capacity(original_size.1));
+        for y in 0..original_size.1 {
+            new_grid[x].push(grid[original_size.0 - x - 1][y]);
+        }
+    }
+
+    new_grid
+}
+
+fn rotate<T>(grid: Vec<Vec<T>>) -> Vec<Vec<T>>
+where
+    T: Copy,
+{
+    let original_size = (grid.len(), grid[0].len());
+
+    let mut new_grid: Vec<Vec<T>> = Vec::with_capacity(original_size.1);
+    for x in 0..original_size.1 {
+        new_grid.push(Vec::with_capacity(original_size.0));
+        for y in 0..original_size.0 {
+            new_grid[x].push(grid[original_size.1 - y - 1][x]);
+        }
+    }
+
+    new_grid
 }
