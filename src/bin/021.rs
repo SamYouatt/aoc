6510c50462d669004c383c104df4e664d5184c9c
Sam Youatt <sdyouatt@btinternet.com>
2021-09-15 21:27:32 +0100

    ✨ worked out all allergen ingredients

--- src/bin/021.rs
+++ src/bin/021.rs
@@ -1,10 +1,13 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    iter::FromIterator,
+};
 
 fn main() {
     let input = include_str!("../../inputs/021.txt");
     let foods = parse_input(input);
 
-    get_allergic_ingredients(&foods);
+    let mapping = get_allergic_ingredients(&foods);
 }
 
 #[derive(Debug)]
@@ -26,14 +29,14 @@ fn parse_input(input: &str) -> Vec<Food> {
         .collect()
 }
 
-fn get_allergic_ingredients<'a>(foods: &[Food]) -> HashMap<&'a str, &'a str> {
+fn get_allergic_ingredients<'a>(foods: &[Food<'a>]) -> HashMap<&'a str, &'a str> {
     // first find the list of an allergen and all the ingerdients it could be in
     // to do this go through the list and each time an allergen appears, check the list of ingredients
     // for that allergen
     // if none exist then put the whole list of ingredients in
     // once it is filled subsequent ones should update the value by findining the intersection of the current ingredients
     // and the new possible ones in the list
-    let allergen_ingredients: HashMap<&str, HashSet<&str>> = foods
+    let mut allergen_ingredients: HashMap<&str, HashSet<&str>> = foods
         .iter()
         .flat_map(|food| food.allergens.iter().map(move |allergen| (food, allergen)))
         .fold(HashMap::new(), |mut map, (food, allergen)| {
@@ -48,9 +51,40 @@ fn get_allergic_ingredients<'a>(foods: &[Food]) -> HashMap<&'a str, &'a str> {
             map
         });
 
-    println!("{:?}", allergen_ingredients);
+    let mut allergen_mappings: HashMap<&str, &str> = HashMap::new();
+    while !allergen_ingredients.is_empty() {
+        let to_remove: Option<(&str, &str)>;
 
-    HashMap::new()
+        println!("remaining allergens: {:?}", allergen_ingredients);
+        // allergen_ingredients.iter().for_each(|allergen| {
+        //     if allergen.1.len() == 1 {
+        //         println!("allergen: {:?}", allergen);
+        //         let ingredient = Vec::from_iter(allergen.1)[0];
+        //         to_remove = Some((allergen.0, ingredient));
+        //         allergen_mappings.insert(allergen.0, *ingredient);
+        //     }
+        // });
+
+        let allergen = allergen_ingredients
+            .iter()
+            .find(|allergen| allergen.1.len() == 1)
+            .unwrap();
+        println!("allergen: {:?}\n", allergen);
+        let ingredient = Vec::from_iter(allergen.1)[0];
+        to_remove = Some((allergen.0, ingredient));
+        allergen_mappings.insert(allergen.0, *ingredient);
+
+        if let Some(to_remove) = to_remove {
+            allergen_ingredients.remove(to_remove.0);
+            for allergen in &mut allergen_ingredients {
+                if allergen.1.contains(to_remove.1) {
+                    allergen.1.remove(to_remove.1);
+                }
+            }
+        }
+    }
+
+    allergen_mappings
 }
 
 #[test]
@@ -60,5 +94,5 @@ trh fvjkl sbzzf mxmxvkd (contains dairy)
 sqjhc fvjkl (contains soy)
 sqjhc mxmxvkd sbzzf (contains fish)";
     let foods = parse_input(input);
-    println!("{:?}", foods[3]);
+    get_allergic_ingredients(&foods);
 }
