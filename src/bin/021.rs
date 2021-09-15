bb0337a6cc0a7ee033f137d197ae13b669beee2a
Sam Youatt <sdyouatt@btinternet.com>
2021-09-15 21:47:54 +0100

    ✨ part 1 completed

--- src/bin/021.rs
+++ src/bin/021.rs
@@ -1,13 +1,33 @@
 use std::{
     collections::{HashMap, HashSet},
     iter::FromIterator,
+    time::Instant,
 };
 
 fn main() {
     let input = include_str!("../../inputs/021.txt");
     let foods = parse_input(input);
 
-    let mapping = get_allergic_ingredients(&foods);
+    let start = Instant::now();
+    println!("Part one: {} in {:#?}", part_one(&foods), start.elapsed());
+}
+
+fn part_one(foods: &[Food]) -> usize {
+    let mapping = get_allergic_ingredients(foods);
+
+    // get a list of all the ingredients that are allergens
+    let allergen_ingredients: Vec<&str> = mapping.into_values().collect();
+
+    // count the number of ingredients in the whole foods list that are not in that allergen list
+    foods
+        .iter()
+        .map(|food| {
+            food.ingredients
+                .iter()
+                .filter(|ingredient| !allergen_ingredients.contains(*ingredient))
+                .count()
+        })
+        .sum::<usize>()
 }
 
 #[derive(Debug)]
@@ -29,13 +49,13 @@ fn parse_input(input: &str) -> Vec<Food> {
         .collect()
 }
 
+// first find the list of an allergen and all the ingerdients it could be in
+// to do this go through the list and each time an allergen appears, check the list of ingredients
+// for that allergen
+// if none exist then put the whole list of ingredients in
+// once it is filled subsequent ones should update the value by findining the intersection of the current ingredients
+// and the new possible ones in the list
 fn get_allergic_ingredients<'a>(foods: &[Food<'a>]) -> HashMap<&'a str, &'a str> {
-    // first find the list of an allergen and all the ingerdients it could be in
-    // to do this go through the list and each time an allergen appears, check the list of ingredients
-    // for that allergen
-    // if none exist then put the whole list of ingredients in
-    // once it is filled subsequent ones should update the value by findining the intersection of the current ingredients
-    // and the new possible ones in the list
     let mut allergen_ingredients: HashMap<&str, HashSet<&str>> = foods
         .iter()
         .flat_map(|food| food.allergens.iter().map(move |allergen| (food, allergen)))
@@ -52,24 +72,15 @@ fn get_allergic_ingredients<'a>(foods: &[Food<'a>]) -> HashMap<&'a str, &'a str>
         });
 
     let mut allergen_mappings: HashMap<&str, &str> = HashMap::new();
+
     while !allergen_ingredients.is_empty() {
         let to_remove: Option<(&str, &str)>;
-
-        println!("remaining allergens: {:?}", allergen_ingredients);
-        // allergen_ingredients.iter().for_each(|allergen| {
-        //     if allergen.1.len() == 1 {
-        //         println!("allergen: {:?}", allergen);
-        //         let ingredient = Vec::from_iter(allergen.1)[0];
-        //         to_remove = Some((allergen.0, ingredient));
-        //         allergen_mappings.insert(allergen.0, *ingredient);
-        //     }
-        // });
 
         let allergen = allergen_ingredients
             .iter()
             .find(|allergen| allergen.1.len() == 1)
             .unwrap();
-        println!("allergen: {:?}\n", allergen);
+
         let ingredient = Vec::from_iter(allergen.1)[0];
         to_remove = Some((allergen.0, ingredient));
         allergen_mappings.insert(allergen.0, *ingredient);
