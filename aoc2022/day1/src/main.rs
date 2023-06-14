0f59516580879cccd50eb283e0001237e6690c03
SamYouatt <sdyouatt@btinternet.com>
2023-06-14 21:24:48 +0100

    Day 1

--- aoc2022/day1/src/main.rs
+++ aoc2022/day1/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("input.txt");
+
+    let answer1: usize = input
+        .split("\n\n")
+        .map(|chunk| (chunk.lines().map(|line| line.parse::<usize>().unwrap())).sum())
+        .max()
+        .unwrap();
+
+    println!("part 1: {}", answer1);
+
+    let mut elf_sums = input
+        .split("\n\n")
+        .map(|chunk| (chunk.lines().map(|line| line.parse::<usize>().unwrap())).sum::<usize>())
+        .collect::<Vec<usize>>();
+
+    elf_sums.sort_unstable();
+
+    let answer2: usize = elf_sums.into_iter().rev().take(3).sum();
+
+    println!("part 2: {}", answer2);
 }
