a1c7dadaf4f9762c8478de009234dcbc789a3adc
SamYouatt <sdyouatt@gmail.com>
2023-12-01 12:06:49 +0000

    part 2

--- aoc2023/day1/src/main.rs
+++ aoc2023/day1/src/main.rs
@@ -1,8 +1,13 @@
+use regex::Regex;
+
 fn main() {
     let input = include_str!("input.txt");
 
     let answer1 = part_1(&input);
     println!("part 1: {}", answer1);
+
+    let answer2 = part_2(&input);
+    println!("part 2: {}", answer2);
 }
 
 fn part_1(input: &str) -> usize {
@@ -14,8 +19,50 @@ fn part_1(input: &str) -> usize {
             let last_digit = digits.last().unwrap_or(&'0');
 
             format!("{}{}", first_digit, last_digit)
-                .parse::<usize>()
+                .parse()
                 .unwrap_or(0)
         })
         .sum()
+}
+
+fn part_2(input: &str) -> usize {
+    let find_digit = Regex::new(r#"\d|one|two|three|four|five|six|seven|eight|nine"#).unwrap();
+    let find_digit_r = Regex::new(r"\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin").unwrap();
+
+    input
+        .lines()
+        .map(|line| {
+            let first_digit =
+                text_to_int(find_digit.find(&line).map(|x| x.as_str()).unwrap_or("0"));
+
+            let last_digit = text_to_int(
+                &find_digit_r
+                    .find(&line.chars().rev().collect::<String>())
+                    .map(|x| x.as_str())
+                    .unwrap_or("0")
+                    .chars()
+                    .rev()
+                    .collect::<String>(),
+            );
+
+            format!("{}{}", first_digit, last_digit)
+                .parse()
+                .unwrap_or(0)
+        })
+        .sum()
+}
+
+fn text_to_int(number: &str) -> usize {
+    match number {
+        "one" | "1" => 1,
+        "two" | "2" => 2,
+        "three" | "3" => 3,
+        "four" | "4" => 4,
+        "five" | "5" => 5,
+        "six" | "6" => 6,
+        "seven" | "7" => 7,
+        "eight" | "8" => 8,
+        "nine" | "9" => 9,
+        _ => 0,
+    }
 }
