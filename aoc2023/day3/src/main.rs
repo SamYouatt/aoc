376a31127adf895bca3efb2d40d825b4c2cd6913
SamYouatt <sdyouatt@gmail.com>
2023-12-03 20:50:00 +0000

    day 3 part 2

--- aoc2023/day3/src/main.rs
+++ aoc2023/day3/src/main.rs
@@ -1,8 +1,8 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use regex::Regex;
 
-#[derive(Debug)]
+#[derive(Debug, Hash, Eq, PartialEq)]
 struct Number {
     occupies: Vec<Location>,
     number: usize,
@@ -34,9 +34,13 @@ impl Number {
                 })
         })
     }
+
+    fn is_at_location(&self, location: &Location) -> bool {
+        self.occupies.iter().any(|loc| loc == location)
+    }
 }
 
-#[derive(Debug, Eq, PartialEq, Hash)]
+#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
 struct Location {
     x: i32,
     y: i32,
@@ -47,6 +51,9 @@ fn main() {
 
     let answer1 = part_1(input);
     println!("Part 1: {}", answer1);
+
+    let answer2 = part_2(input);
+    println!("Part 2: {}", answer2);
 }
 
 fn part_1(input: &str) -> usize {
@@ -82,4 +89,64 @@ fn part_1(input: &str) -> usize {
         })
         .filter(|num| num.is_adjacent(&symbols))
         .fold(0, |total, num| total + num.number)
+}
+
+fn part_2(input: &str) -> usize {
+    let match_gear = Regex::new(r"\*").expect("Failed to compile regex");
+
+    let gears: HashSet<Location> = input
+        .lines()
+        .enumerate()
+        .flat_map(|(row_num, line)| {
+            match_gear
+                .find_iter(line)
+                .map(move |symbol_match| Location {
+                    x: symbol_match.start() as i32,
+                    y: row_num as i32,
+                })
+        })
+        .collect();
+
+    let match_number = Regex::new(r"\d+").expect("Failed to compile regex");
+
+    let numbers: Vec<Number> = input
+        .lines()
+        .enumerate()
+        .flat_map(|(row_num, line)| {
+            match_number.find_iter(line).map(move |num_match| {
+                Number::new(
+                    num_match.start(),
+                    num_match.end() - 1,
+                    row_num,
+                    num_match.as_str().parse().expect("Failed to parse number"),
+                )
+            })
+        })
+        .collect();
+
+    let mut gears_to_numbers: HashMap<Location, HashSet<&Number>> = HashMap::new();
+
+    for gear in gears {
+        for x_off in -1..=1 {
+            for y_off in -1..=1 {
+                let location = Location {
+                    x: gear.x + x_off,
+                    y: gear.y + y_off,
+                };
+                if let Some(number) = numbers.iter().find(|num| num.is_at_location(&location)) {
+                    gears_to_numbers
+                        .entry(gear)
+                        .or_insert_with(HashSet::new)
+                        .insert(number);
+                }
+            }
+        }
+    }
+
+    gears_to_numbers
+        .iter()
+        .filter(|(_, numbers)| numbers.len() == 2)
+        .fold(0, |total, (_, numbers)| {
+            total + numbers.into_iter().map(|num| num.number).product::<usize>()
+        })
 }
