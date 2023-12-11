1dc8604b9c86191a37a979358567670e68577e21
SamYouatt <sdyouatt@gmail.com>
2023-12-11 08:07:08 +0000

    day 11 part 2

--- aoc2023/day11/src/main.rs
+++ aoc2023/day11/src/main.rs
@@ -5,9 +5,28 @@ fn main() {
 
     let answer1 = part_1(input);
     println!("Part 1: {answer1}");
+
+    let answer2 = part_2(input);
+    println!("Part 2: {answer2}");
 }
 
 fn part_1(input: &str) -> isize {
+    let (grid, mut galaxies) = parse_grid(input);
+
+    expand_coords(&mut galaxies, &grid, 2);
+
+    sum_mc_distances(&galaxies)
+}
+
+fn part_2(input: &str) -> isize {
+    let (grid, mut galaxies) = parse_grid(input);
+
+    expand_coords(&mut galaxies, &grid, 1_000_000);
+
+    sum_mc_distances(&galaxies)
+}
+
+fn parse_grid(input: &str) -> (Vec<Vec<bool>>, Vec<(usize, usize)>) {
     let mut galaxies = vec![];
 
     let grid: Vec<Vec<_>> = input
@@ -26,25 +45,38 @@ fn part_1(input: &str) -> isize {
         })
         .collect();
 
-    let empty_rows = (0..grid.len()).filter(|&row| grid[row].iter().all(|is_star| !is_star));
-    let empty_cols = (0..grid[0].len()).filter(|&col| (0..grid.len()).all(|row| !grid[row][col]));
+    (grid, galaxies)
+}
 
-    for row in empty_rows.rev() {
-        for galaxy in &mut galaxies {
+fn expand_coords(
+    galaxies: &mut Vec<(usize, usize)>,
+    grid: &Vec<Vec<bool>>,
+    expansion_factor: usize,
+) {
+    let empty_rows: Vec<_> = (0..grid.len())
+        .filter(|&row| grid[row].iter().all(|is_star| !is_star))
+        .collect();
+
+    let empty_cols: Vec<_> = (0..grid[0].len())
+        .filter(|&col| (0..grid.len()).all(|row| !grid[row][col]))
+        .collect();
+
+    for galaxy in &mut *galaxies {
+        for &row in empty_rows.iter().rev() {
             if galaxy.1 > row {
-                galaxy.1 += 1;
+                galaxy.1 += expansion_factor - 1;
             }
         }
-    }
 
-    for col in empty_cols.rev() {
-        for galaxy in &mut galaxies {
+        for &col in empty_cols.iter().rev() {
             if galaxy.0 > col {
-                galaxy.0 += 1;
+                galaxy.0 += expansion_factor - 1;
             }
         }
     }
+}
 
+fn sum_mc_distances(galaxies: &Vec<(usize, usize)>) -> isize {
     galaxies
         .iter()
         .tuple_combinations()
