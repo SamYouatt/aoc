306e0d7971f023575021bc35df94486170275f8e
SamYouatt <sdyouatt@gmail.com>
2023-12-12 11:22:20 +0000

    day 12 part 2

--- aoc2023/day12/src/main.rs
+++ aoc2023/day12/src/main.rs
@@ -1,11 +1,18 @@
+use std::collections::{hash_map::DefaultHasher, HashMap};
+use std::hash::{Hash, Hasher};
+
 fn main() {
     let input = include_str!("input.txt");
 
     let answer1 = part_1(input);
     println!("Part 1: {answer1}");
+
+    let answer2 = part_2(input);
+    println!("Part 2: {answer2}");
 }
 
 fn part_1(input: &str) -> usize {
+    let mut cache = HashMap::new();
     input
         .lines()
         .map(|line| {
@@ -16,7 +23,34 @@ fn part_1(input: &str) -> usize {
                 .map(|num| num.parse::<usize>().unwrap())
                 .collect();
 
-            possible_arrangements(&squares, &counts, None)
+            possible_arrangements(&squares, &counts, None, &mut cache)
+        })
+        .sum()
+}
+
+fn part_2(input: &str) -> usize {
+    let mut cache = HashMap::new();
+    input
+        .lines()
+        .map(|line| {
+            let (squares, counts) = line.split_once(' ').unwrap();
+            let squares: Vec<_> = squares.chars().collect();
+            let counts: Vec<_> = counts
+                .split(',')
+                .map(|num| num.parse::<usize>().unwrap())
+                .collect();
+
+            let mut repeated_squares = squares.clone();
+            let mut repeated_counts = counts.clone();
+
+            for _ in 0..4 {
+                repeated_squares.push('?');
+                repeated_squares.append(&mut squares.clone());
+
+                repeated_counts.append(&mut counts.clone());
+            }
+
+            possible_arrangements(&repeated_squares, &repeated_counts, None, &mut cache)
         })
         .sum()
 }
@@ -27,7 +61,13 @@ fn possible_arrangements(
     squares: &[char],
     counts: &[usize],
     current_group: Option<usize>,
+    cache: &mut HashMap<u64, usize>,
 ) -> usize {
+    let key = get_hash_key(squares, counts, current_group);
+    if let Some(&cached) = cache.get(&key) {
+        return cached;
+    }
+
     // Base case: reached the end of the squares, nothing to place, and not in a group -> Valid
     // configuration
     if squares.is_empty() && counts.is_empty() && current_group.is_none() {
@@ -62,31 +102,38 @@ fn possible_arrangements(
         // Currently in a group and there is more in that group to place but have reached an empty
         ('.', Some(x)) if x != counts[0] => 0,
         // Otherwise we have finished that group so exit it and move onto the next group count
-        ('.', Some(_)) => possible_arrangements(&squares[1..], &counts[1..], None),
+        ('.', Some(_)) => possible_arrangements(&squares[1..], &counts[1..], None, cache),
         // Not in a group and hit an empty, just continue to the next square
-        ('.', None) => possible_arrangements(&squares[1..], counts, None),
+        ('.', None) => possible_arrangements(&squares[1..], counts, None, cache),
         // We hit a tile and now enter a new group
-        ('#', None) => possible_arrangements(&squares[1..], counts, Some(1)),
+        ('#', None) => possible_arrangements(&squares[1..], counts, Some(1), cache),
         // We are in a group and find a placed tile to bump the current group by 1
-        ('#', Some(_)) => {
-            possible_arrangements(&squares[1..], counts, current_group.map(|count| count + 1))
-        }
+        ('#', Some(_)) => possible_arrangements(
+            &squares[1..],
+            counts,
+            current_group.map(|count| count + 1),
+            cache,
+        ),
         // We encounter an option and we aren't in a group so the two options from here are to not
         // place a tile and not enter a group, or place a tile and enter a new group
         ('?', None) => {
-            possible_arrangements(&squares[1..], counts, None)
-                + possible_arrangements(&squares[1..], counts, Some(1))
+            possible_arrangements(&squares[1..], counts, None, cache)
+                + possible_arrangements(&squares[1..], counts, Some(1), cache)
         }
         // We have an option and we are in a group. Find the configs if we continue in that group,
         // then if we have finished the current group, start searching for the next group and
         // include those configs as well
         ('?', Some(x)) => {
-            let continuing_configs =
-                possible_arrangements(&squares[1..], counts, current_group.map(|count| count + 1));
+            let continuing_configs = possible_arrangements(
+                &squares[1..],
+                counts,
+                current_group.map(|count| count + 1),
+                cache,
+            );
 
             if x == counts[0] {
                 return continuing_configs
-                    + possible_arrangements(&squares[1..], &counts[1..], None);
+                    + possible_arrangements(&squares[1..], &counts[1..], None, cache);
             }
 
             return continuing_configs;
@@ -94,5 +141,14 @@ fn possible_arrangements(
         _ => panic!("Unexpected option"),
     };
 
+    cache.insert(key, configurations);
     configurations
+}
+
+fn get_hash_key(squares: &[char], counts: &[usize], current_group: Option<usize>) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    squares.hash(&mut hasher);
+    counts.hash(&mut hasher);
+    current_group.hash(&mut hasher);
+    hasher.finish()
 }
