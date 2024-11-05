4a66257e30b5f6644d2c23f702c13bef474ebfcd
SamYouatt <sdyouatt@gmail.com>
2024-11-05 08:34:35 +0000

    green: get the tests passing by processing 1,2,99 opcodes

--- aoc2019/intcomputer/src/lib.rs
+++ aoc2019/intcomputer/src/lib.rs
@@ -1,13 +1,20 @@
+// Opcodes:
+// 99 - Halt
+// 1 - adds the two following reference's values and places in third's referenced location
+// 2 - multiplies the two following reference's values and places in third's referenced location
+
 type Tape = Vec<usize>;
 
 pub struct Computer {
     tape: Tape,
+    head: usize,
 }
 
 impl Computer {
     pub fn new(tape: &Tape) -> Self {
         Self {
             tape: tape.to_owned(),
+            head: 0,
         }
     }
 
@@ -18,11 +25,52 @@ impl Computer {
 
     /// Runs the program until it halts
     pub fn run(&mut self) {
-        todo!()
+        while self.tape[self.head] != 99 {
+            match self.tape[self.head] {
+                1 => {
+                    let first = self.deref_index(self.head + 1);
+                    let second = self.deref_index(self.head + 2);
+
+                    let result = first + second;
+
+                    let result_loc = self.tape[self.head + 3];
+                    self.tape[result_loc] = result;
+                }
+                2 => {
+                    let first = self.deref_index(self.head + 1);
+                    let second = self.deref_index(self.head + 2);
+
+                    let result = first * second;
+
+                    let result_loc = self.tape[self.head + 3];
+                    self.tape[result_loc] = result;
+                }
+                99 => unreachable!(),
+                _ => panic!("unsupported opcode"),
+            }
+
+            self.advance();
+        }
+    }
+
+    /// Given a location in the tape, will inspect that value then follow it to the memory address
+    fn deref_index(&self, reference_index: usize) -> usize {
+        let reference = self.tape[reference_index];
+        let deref = self.tape[reference];
+
+        deref
+    }
+
+    /// Advance to the next opcode
+    fn advance(&mut self) {
+        self.head += 4;
     }
 }
 
 /// Expects comma separated list of numbers
 pub fn parse_tape(input: &str) -> Tape {
-    todo!()
+    input
+        .split(',')
+        .map(|x| x.parse().expect("expected only integers in input"))
+        .collect()
 }
