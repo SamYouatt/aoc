68f9c23d122789cbe39045401cba09b7559b332f
SamYouatt <sdyouatt@gmail.com>
2024-11-05 09:07:40 +0000

    day 2 part 1

--- aoc2019/intcomputer/src/main.rs
+++ aoc2019/intcomputer/src/main.rs
@@ -1,11 +1,20 @@
 use intcomputer::{parse_tape, Computer};
 
 fn main() {
+    println!("Day 2 part 1: {}", day2());
+}
+
+fn day2() -> usize {
     let day2_input = include_str!("inputs/day2_input.txt");
 
-    let tape = parse_tape(day2_input);
+    let mut tape = parse_tape(day2_input);
+    tape[1] = 12;
+    tape[2] = 2;
 
-    let computer = Computer::new(&tape);
+    let mut computer = Computer::new(&tape);
+    computer.run();
+    
+    let final_tape = computer.dump_tape();
 
-    todo!()
+    final_tape[0]
 }
