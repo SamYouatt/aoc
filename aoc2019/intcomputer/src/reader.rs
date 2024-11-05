211d4471290ebcaa58dee7fa46df1823fae7600c
SamYouatt <sdyouatt@gmail.com>
2024-11-05 19:37:47 +0000

    trim the value from std in it has newline on end

--- aoc2019/intcomputer/src/reader.rs
+++ aoc2019/intcomputer/src/reader.rs
@@ -8,13 +8,13 @@ pub struct StdInReader;
 
 impl Reader for StdInReader {
     fn read_input(&mut self) -> i64 {
-        println!(">>");
+        println!("v Input v");
 
         let mut input = String::new();
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read line");
 
-        input.parse().expect("input should be valid i64")
+        input.trim().parse().expect("input should be valid i64")
     }
 }
