617b256dc5dcfb409d93bd120b4175d212b18ec8
SamYouatt <sdyouatt@gmail.com>
2024-11-05 09:22:04 +0000

    rename new -> load for funsies

--- aoc2019/intcomputer/src/main.rs
+++ aoc2019/intcomputer/src/main.rs
@@ -12,7 +12,7 @@ fn day2_part1(input: &str) -> usize {
     tape[1] = 12;
     tape[2] = 2;
 
-    let mut computer = Computer::new(&tape);
+    let mut computer = Computer::load(&tape);
     computer.run();
 
     let final_tape = computer.dump_tape();
@@ -29,7 +29,7 @@ fn day2_part2(input: &str) -> usize {
         tape[1] = noun;
         tape[2] = verb;
 
-        let mut computer = Computer::new(&tape);
+        let mut computer = Computer::load(&tape);
         computer.run();
 
         let final_tape = computer.dump_tape();
