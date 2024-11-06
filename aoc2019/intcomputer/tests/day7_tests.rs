bb5ad81c417a99ea36cd500ef28741c90edaef50
SamYouatt <sdyouatt@gmail.com>
2024-11-06 17:14:27 +0000

    add this test for confidence as well

--- aoc2019/intcomputer/tests/day7_tests.rs
+++ aoc2019/intcomputer/tests/day7_tests.rs
@@ -14,3 +14,16 @@ fn example_that_produces_self() {
 
     assert!(computer.dump_tape().starts_with(&tape));
 }
+
+#[test]
+fn example_that_makes_large_middle_num() {
+    let input = "104,1125899906842624,99";
+    let tape = parse_tape(input);
+    let (_in_sender, in_receiver) = mpsc::channel();
+    let (out_sender, out_receiver) = mpsc::channel();
+
+    let mut computer = Computer::load(&tape, in_receiver, out_sender);
+    computer.run();
+
+    assert_eq!(out_receiver.recv().expect("no output"), tape[1]); 
+}
