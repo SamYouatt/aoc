e5be8844bf2f515d95b13666099826ab77b999ac
SamYouatt <sdyouatt@gmail.com>
2024-11-06 14:22:56 +0000

    complete: day 7 part 2

--- aoc2019/intcomputer/src/main.rs
+++ aoc2019/intcomputer/src/main.rs
@@ -37,6 +37,7 @@ fn main() {
         (5, 1) => println!("Day 5 part 1: {}", day5(day5_input, 1)),
         (5, 2) => println!("Day 5 part 2: {}", day5(day5_input, 5)),
         (7, 1) => println!("Day 7 part 1: {}", day7_part1(day7_input)),
+        (7, 2) => println!("Day 7 part 2: {}", day7_part2(day7_input)),
         _ => eprintln!("Pick a proper day and part fool"),
     }
 }
@@ -99,7 +100,7 @@ fn day7_part1(input: &str) -> usize {
 
     for permutation in (0i64..5).permutations(5) {
         let (mut current_sender, mut current_receiver) = mpsc::channel();
-        // Used to kickstart process
+        // Challenge says to feed A a 0 to start the process so hang onto this
         let first_sender = current_sender.clone();
         let mut amplifiers = Vec::new();
 
@@ -130,4 +131,57 @@ fn day7_part1(input: &str) -> usize {
     }
 
     max_signal as usize
+}
+
+fn day7_part2(input: &str) -> usize {
+    let tape = parse_tape(input);
+    let (results_send, results_recv) = mpsc::channel();
+
+    for permutation in (5i64..10).permutations(5) {
+        let (mut current_sender, mut current_receiver) = mpsc::channel();
+        // Challenge says to feed A a 0 to start the process so hang onto this
+        let first_sender = current_sender.clone();
+
+        for (i, phase_setting) in permutation.iter().enumerate() {
+            current_sender
+                .send(*phase_setting)
+                .expect("send should never close");
+
+            let (mut next_sender, next_receiver) = mpsc::channel();
+
+            // Final circuit needs to feed back to beginning
+            if i == 4 {
+                next_sender = first_sender.clone();
+            }
+
+            let mut amplifier = Computer::load(&tape, current_receiver, next_sender.clone());
+            let results_send = results_send.clone();
+
+            thread::spawn(move || {
+                amplifier.run();
+
+                // A's final position should have one message left on the queue - which is the
+                // output for that, alternative here is to make the final sender for E be a
+                // broadcast, or allow each computer to take multiple senders
+                if i == 0 {
+                    let unused_message = amplifier.receiver().recv();
+                    results_send.send(unused_message).expect("results send should not close");
+                }
+            });
+
+            (current_receiver, current_sender) = (next_receiver, next_sender);
+        }
+
+        // Kickstart the circuit
+        first_sender
+            .send(0)
+            .expect("first sender should never close");
+    }
+
+    drop(results_send);
+    results_recv
+        .iter()
+        .map(|x| x.expect("results recv should not close"))
+        .max()
+        .expect("no values output") as usize
 }
