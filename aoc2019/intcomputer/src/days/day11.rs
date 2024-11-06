e92cd87ab1c3a08a0589d2aa991d466956cc64f2
SamYouatt <sdyouatt@gmail.com>
2024-11-06 22:27:00 +0000

    refactor: more functional and better file ordering

--- aoc2019/intcomputer/src/days/day11.rs
+++ aoc2019/intcomputer/src/days/day11.rs
@@ -16,27 +16,6 @@ impl Point {
     pub fn new(x: i64, y: i64) -> Self {
         Self { x, y }
     }
-
-    pub fn forwards(self, direction: &Direction) -> Self {
-        match direction {
-            Direction::Up => Self {
-                x: self.x,
-                y: self.y + 1,
-            },
-            Direction::Down => Self {
-                x: self.x,
-                y: self.y - 1,
-            },
-            Direction::Left => Self {
-                x: self.x - 1,
-                y: self.y,
-            },
-            Direction::Right => Self {
-                x: self.x + 1,
-                y: self.y,
-            },
-        }
-    }
 }
 
 #[derive(Debug)]
@@ -50,60 +29,6 @@ enum Direction {
     Down,
     Left,
     Right,
-}
-
-fn rotate(current: &Direction, clockwise: bool) -> Direction {
-    match (current, clockwise) {
-        (Direction::Up, true) | (Direction::Down, false) => Direction::Right,
-        (Direction::Up, false) | (Direction::Down, true) => Direction::Left,
-        (Direction::Left, true) | (Direction::Right, false) => Direction::Up,
-        (Direction::Left, false) | (Direction::Right, true) => Direction::Down,
-    }
-}
-
-fn colour_message(position: &Point, grid: &HashMap<Point, Colour>) -> i64 {
-    match grid.get(position) {
-        Some(Colour::White) => 1,
-        Some(Colour::Black) | None => 0,
-    }
-}
-
-fn robot(sender: Sender<i64>, receiver: Receiver<i64>, grid: &mut HashMap<Point, Colour>) {
-    let mut position = Point::new(0, 0);
-    let mut facing = Direction::Up;
-    let mut painting = true;
-
-    sender
-        .send(colour_message(&position, &grid))
-        .expect("computer should not close receiver");
-
-    while let Ok(instruction) = receiver.recv() {
-        match painting {
-            true => {
-                let colour = match instruction {
-                    0 => Colour::Black,
-                    1 => Colour::White,
-                    _ => panic!("unexpected instruction from computer"),
-                };
-                grid.insert(position, colour);
-                painting = false;
-            }
-            false => {
-                // 0 = anti-clockwise, 1 = clockwise
-                facing = match instruction {
-                    0 => rotate(&facing, false),
-                    1 => rotate(&facing, true),
-                    _ => panic!("unexpected instruction from computer"),
-                };
-
-                position = position.forwards(&facing);
-
-                // Computer may have halted and closed its receiver but we don't care
-                let _ = sender.send(colour_message(&position, &grid));
-                painting = true;
-            }
-        }
-    }
 }
 
 pub fn part1(input: &str) -> usize {
@@ -125,4 +50,67 @@ pub fn part1(input: &str) -> usize {
     let visited: HashSet<Point> = grid.keys().map(|p| *p).collect();
 
     visited.len()
+}
+
+fn robot(sender: Sender<i64>, receiver: Receiver<i64>, grid: &mut HashMap<Point, Colour>) {
+    let mut position = Point::new(0, 0);
+    let mut facing = Direction::Up;
+    let mut painting = true;
+
+    sender
+        .send(encode_colour(&position, &grid))
+        .expect("computer should not close receiver");
+
+    while let Ok(instruction) = receiver.recv() {
+        match painting {
+            true => {
+                let colour = match instruction {
+                    0 => Colour::Black,
+                    1 => Colour::White,
+                    _ => panic!("unexpected instruction from computer"),
+                };
+                grid.insert(position, colour);
+                painting = false;
+            }
+            false => {
+                // 0 = anti-clockwise, 1 = clockwise
+                facing = match instruction {
+                    0 => rotate(&facing, false),
+                    1 => rotate(&facing, true),
+                    _ => panic!("unexpected instruction from computer"),
+                };
+
+                position = forwards(&position, &facing);
+
+                // Computer may have halted and closed its receiver but we don't care
+                let _ = sender.send(encode_colour(&position, &grid));
+                painting = true;
+            }
+        }
+    }
+}
+
+fn rotate(current: &Direction, clockwise: bool) -> Direction {
+    match (current, clockwise) {
+        (Direction::Up, true) | (Direction::Down, false) => Direction::Right,
+        (Direction::Up, false) | (Direction::Down, true) => Direction::Left,
+        (Direction::Left, true) | (Direction::Right, false) => Direction::Up,
+        (Direction::Left, false) | (Direction::Right, true) => Direction::Down,
+    }
+}
+
+fn encode_colour(position: &Point, grid: &HashMap<Point, Colour>) -> i64 {
+    match grid.get(position) {
+        Some(Colour::White) => 1,
+        Some(Colour::Black) | None => 0,
+    }
+}
+
+fn forwards(from: &Point, direction: &Direction) -> Point {
+    match direction {
+        Direction::Up => Point::new(from.x, from.y + 1),
+        Direction::Down => Point::new(from.x, from.y - 1),
+        Direction::Left => Point::new(from.x - 1, from.y),
+        Direction::Right => Point::new(from.x + 1, from.y),
+    }
 }
