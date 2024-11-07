8eaac1185ea20ada91ad2115739be0906d47e2ab
SamYouatt <sdyouatt@gmail.com>
2024-11-07 12:48:53 +0000

    refactor: move point into lib
    will need it for day 13 as well

--- aoc2019/intcomputer/src/lib.rs
+++ aoc2019/intcomputer/src/lib.rs
@@ -1,2 +1,14 @@
-pub mod days;
 pub mod computer;
+pub mod days;
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
+pub struct Point {
+    x: i64,
+    y: i64,
+}
+
+impl Point {
+    pub fn new(x: i64, y: i64) -> Self {
+        Self { x, y }
+    }
+}
