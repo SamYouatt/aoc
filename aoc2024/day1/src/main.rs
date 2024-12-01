39b0948e2dd7bb294104054d0be9960c4d0bda4c
SamYouatt <sdyouatt@gmail.com>
2024-12-01 09:45:28 +0000

    part 2

--- aoc2024/day1/src/main.rs
+++ aoc2024/day1/src/main.rs
@@ -1,7 +1,10 @@
+use std::collections::HashMap;
+
 fn main() {
     let input = include_str!("input.txt");
 
     println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
 
 fn part1(input: &str) -> usize {
@@ -18,5 +21,23 @@ fn part1(input: &str) -> usize {
         .iter()
         .zip(list_y.iter())
         .map(|(x, y)| x.abs_diff(*y))
+        .sum()
+}
+
+fn part2(input: &str) -> usize {
+    let (list_x, list_y): (Vec<_>, Vec<_>) = input
+        .lines()
+        .map(|line| line.split_once("   ").unwrap())
+        .map(|(x, y)| (x.parse::<usize>().unwrap(), y.parse::<usize>().unwrap()))
+        .unzip();
+
+    let list_y_freq = list_y.into_iter().fold(HashMap::new(), |mut acc, y| {
+        *acc.entry(y).or_insert(0) += 1;
+        acc
+    });
+
+    list_x
+        .iter()
+        .map(|x| x * list_y_freq.get(x).unwrap_or(&0))
         .sum()
 }
