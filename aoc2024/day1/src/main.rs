9aef7b972eb9c0bc4c52a9e6cc619307c3842455
SamYouatt <sdyouatt@gmail.com>
2024-12-01 09:35:58 +0000

    part 1

--- aoc2024/day1/src/main.rs
+++ aoc2024/day1/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("input.txt");
+
+    println!("Part 1: {}", part1(&input));
+}
+
+fn part1(input: &str) -> usize {
+    let (mut list_x, mut list_y): (Vec<_>, Vec<_>) = input
+        .lines()
+        .map(|line| line.split_once("   ").unwrap())
+        .map(|(x, y)| (x.parse::<usize>().unwrap(), y.parse::<usize>().unwrap()))
+        .unzip();
+
+    list_x.sort();
+    list_y.sort();
+
+    list_x
+        .iter()
+        .zip(list_y.iter())
+        .map(|(x, y)| x.abs_diff(*y))
+        .sum()
 }
