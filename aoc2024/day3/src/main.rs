222ca6b475692651018cfb135517084ed91c126a
SamYouatt <sdyouatt@gmail.com>
2024-12-03 07:51:36 +0000

    day 3 part 2

--- aoc2024/day3/src/main.rs
+++ aoc2024/day3/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     let input = include_str!("input.txt");
 
     println!("Part 1: {}", part_1(input));
+    println!("Part 2: {}", part_2(input));
 }
 
 fn part_1(input: &str) -> usize {
@@ -17,4 +18,34 @@ fn part_1(input: &str) -> usize {
             (x.parse::<usize>().unwrap(), y.parse::<usize>().unwrap())
         })
         .fold(0, |acc, (x, y)| acc + (x * y))
+}
+
+fn part_2(input: &str) -> usize {
+    let regex =
+        regex::Regex::new(r#"mul\((\d+,\d+)\)|(do\(\))|(don't\(\))"#).expect("invalid regex dummy");
+
+    let mut total = 0;
+    let mut enabled = true;
+
+    for capture in regex.captures_iter(input).map(|c| c.extract::<1>().0) {
+        if capture.starts_with("do(") {
+            enabled = true;
+            continue;
+        }
+        if capture.starts_with("don't") {
+            enabled = false;
+            continue;
+        }
+
+        if enabled {
+            let (_, rem) = capture.split_once('(').unwrap();
+            let (rem, _) = rem.split_once(')').unwrap();
+            let (x, y) = rem.split_once(',').unwrap();
+            let (x, y) = (x.parse::<usize>().unwrap(), y.parse::<usize>().unwrap());
+
+            total += x * y;
+        }
+    }
+
+    total
 }
