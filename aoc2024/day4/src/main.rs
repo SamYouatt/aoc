3c0e4dc9b29a32d4540c2c80df08b324d4ebad3a
SamYouatt <sdyouatt@gmail.com>
2024-12-04 08:14:13 +0000

    day 4 part 2

--- aoc2024/day4/src/main.rs
+++ aoc2024/day4/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     let input = include_str!("input.txt");
 
     println!("Part 1: {}", part_1(input));
+    println!("Part 2: {}", part_2(input));
 }
 
 fn part_1(input: &str) -> usize {
@@ -110,6 +111,45 @@ fn part_1(input: &str) -> usize {
     total
 }
 
+fn part_2(input: &str) -> usize {
+    let grid = input
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect::<Vec<Vec<_>>>();
+
+    let mut total = 0;
+    for (row_i, row) in grid.iter().enumerate() {
+        for (col_i, &char) in row.iter().enumerate() {
+            if char != 'A' {
+                continue;
+            }
+
+            if !row_i.checked_sub(1).is_some()
+                || !col_i.checked_sub(1).is_some()
+                || !grid.get(row_i + 1).is_some()
+                || !row.get(col_i + 1).is_some()
+            {
+                continue;
+            }
+
+            match (
+                grid[row_i - 1][col_i - 1],
+                grid[row_i - 1][col_i + 1],
+                grid[row_i + 1][col_i + 1],
+                grid[row_i + 1][col_i - 1],
+            ) {
+                ('M', 'M', 'S', 'S')
+                | ('S', 'M', 'M', 'S')
+                | ('S', 'S', 'M', 'M')
+                | ('M', 'S', 'S', 'M') => total += 1,
+                _ => continue,
+            }
+        }
+    }
+
+    total
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -128,5 +168,21 @@ MAMMMXMMMM
 MXMXAXMASX";
 
         assert_eq!(part_1(input), 18);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
+
+        assert_eq!(part_2(input), 9);
     }
 }
