d7c3f6685dacbb5d709ba9c5627f5934fae30048
SamYouatt <sdyouatt@gmail.com>
2024-12-05 16:26:15 +0000

    simplify both parts by introducing sorting logic for built in sort_by

--- aoc2024/day5/src/main.rs
+++ aoc2024/day5/src/main.rs
@@ -1,58 +1,37 @@
-use itertools::Itertools;
+use std::collections::HashMap;
 
 fn main() {
     let input = include_str!("input.txt");
-    let (mut pages, rules) = parse(input);
+    let (mut pages, page_to_preds) = parse(input);
 
-    println!("Part 1: {}", part_1(&pages, &rules));
-    println!("Part 2: {}", part_2(&mut pages, &rules));
+    println!("Part 1: {}", part_1(&pages, &page_to_preds));
+    println!("Part 2: {}", part_2(&mut pages, &page_to_preds));
 }
 
-fn part_1(pages: &Vec<Vec<usize>>, rules: &Vec<(usize, usize)>) -> usize {
+fn part_1(pages: &Vec<Vec<usize>>, page_to_pred: &HashMap<usize, Vec<usize>>) -> usize {
     pages
-        .into_iter()
-        .filter(|pages| is_valid_pages(pages, &rules))
+        .iter()
+        .filter(|pages| pages.is_sorted_by(|a, b| page_to_pred[b].contains(a)))
         .map(|pages| pages[(pages.len() - 1) / 2])
         .sum()
 }
 
-fn part_2(pages: &mut Vec<Vec<usize>>, rules: &Vec<(usize, usize)>) -> usize {
-    let mut bad_pages: Vec<_> = pages
+fn part_2(all_pages: &mut Vec<Vec<usize>>, page_to_pred: &HashMap<usize, Vec<usize>>) -> usize {
+    let bad_pages = all_pages
         .into_iter()
-        .filter(|pages| !is_valid_pages(pages, &rules))
-        .collect();
+        .filter(|pages| !pages.is_sorted_by(|a, b| page_to_pred[b].contains(a)))
+        .collect::<Vec<_>>();
 
-    for pages in bad_pages.iter_mut() {
-        while !is_valid_pages(pages, rules) {
-            for (x_i, y_i) in (0..pages.len())
-                .collect::<Vec<usize>>()
-                .into_iter()
-                .tuple_windows()
-            {
-                let x = pages[x_i];
-                let y = pages[y_i];
-
-                for &rule in rules {
-                    if (x != rule.0 && x != rule.1) || (y != rule.0 && y != rule.1) {
-                        continue;
-                    }
-
-                    // I think its fine to just test the direct pairs and not think further out?
-                    if x == rule.1 && y == rule.0 {
-                        pages.swap(x_i, y_i);
-                    }
-                }
-            }
-        }
+    let mut total = 0;
+    for pages in bad_pages {
+        pages.sort_by(|a, b| page_to_pred[b].contains(a).cmp(&true));
+        total += pages[(pages.len() - 1) / 2];
     }
 
-    bad_pages
-        .iter()
-        .map(|pages| pages[(pages.len() - 1) / 2])
-        .sum()
+    return total;
 }
 
-fn parse(input: &str) -> (Vec<Vec<usize>>, Vec<(usize, usize)>) {
+fn parse(input: &str) -> (Vec<Vec<usize>>, HashMap<usize, Vec<usize>>) {
     let (rules, pages) = input.split_once("\n\n").unwrap();
 
     let rules: Vec<(usize, usize)> = rules
@@ -70,24 +49,12 @@ fn parse(input: &str) -> (Vec<Vec<usize>>, Vec<(usize, usize)>) {
         })
         .collect();
 
-    (pages, rules)
-}
-
-fn is_valid_pages(pages: &Vec<usize>, rules: &Vec<(usize, usize)>) -> bool {
-    for (&x, &y) in pages.iter().tuple_windows() {
-        for &rule in rules {
-            if (x != rule.0 && x != rule.1) || (y != rule.0 && y != rule.1) {
-                continue;
-            }
-
-            // I think its fine to just test the direct pairs and not think further out?
-            if x == rule.1 && y == rule.0 {
-                return false;
-            }
-        }
+    let mut page_to_preds = HashMap::new();
+    for rule in rules.iter() {
+        page_to_preds.entry(rule.1).or_insert(vec![]).push(rule.0);
     }
 
-    true
+    (pages, page_to_preds)
 }
 
 #[cfg(test)]
@@ -125,42 +92,42 @@ mod tests {
 61,13,29
 97,13,75,29,47";
 
-        let (pages, rules) = parse(input);
-        assert_eq!(part_1(&pages, &rules), 143);
+        let (pages, map) = parse(input);
+        assert_eq!(part_1(&pages, &map), 143);
     }
 
     #[test]
     fn test_part_2() {
         let input = "47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
+    97|13
+    97|61
+    97|47
+    75|29
+    61|13
+    75|53
+    29|13
+    97|29
+    53|29
+    61|53
+    97|53
+    61|29
+    47|13
+    75|47
+    97|75
+    47|61
+    75|61
+    47|29
+    75|13
+    53|13
 
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47";
+    75,47,61,53,29
+    97,61,53,29,13
+    75,29,13
+    75,97,47,61,53
+    61,13,29
+    97,13,75,29,47";
 
-        let (mut pages, rules) = parse(input);
-        assert_eq!(part_2(&mut pages, &rules), 123);
+        let (mut pages, map) = parse(input);
+        assert_eq!(part_2(&mut pages, &map), 123);
     }
 }
