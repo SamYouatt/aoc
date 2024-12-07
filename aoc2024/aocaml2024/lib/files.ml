a86cca51ec330c8fc3dcec8a5b9cd0bf4ca93dd0
SamYouatt <sdyouatt@gmail.com>
2024-12-07 17:26:17 +0000

    create lines function that returns list of each line in the file

--- aoc2024/aocaml2024/lib/files.ml
+++ aoc2024/aocaml2024/lib/files.ml
@@ -1,1 +1,10 @@
 let readfile filename = Printf.printf "filename: %s\n" filename
+
+let lines filename =
+    let channel = open_in filename in
+    let rec build_lines lines =
+        match input_line channel with
+        | line -> build_lines (line :: lines)
+        | exception End_of_file -> close_in channel; List.rev lines
+    in
+    build_lines []
