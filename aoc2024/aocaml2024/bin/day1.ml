79857f0519385cd9a29fa4c2d81cc794cbb5e4c6
SamYouatt <sdyouatt@gmail.com>
2024-12-07 20:44:09 +0000

    play around with the pipe operator

--- aoc2024/aocaml2024/bin/day1.ml
+++ aoc2024/aocaml2024/bin/day1.ml
@@ -1,6 +1,5 @@
 let () =
-  let lines = Files.lines "inputs/day1.txt" in
-  List.iter (fun line -> match Strings.split_once "   " line with
-  | Some(x, y) -> Printf.printf "%s %s\n" x y
-  | None -> ()
-) lines
+    Files.lines "inputs/day1.txt"
+    |> List.filter_map (fun line -> Strings.split_once "   " line)
+    |> List.iter (fun (first, second) -> Printf.printf "%s %s\n" first second)
+
