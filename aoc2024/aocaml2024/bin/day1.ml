eaef247c558a2adacca94ba861e73edd31257983
SamYouatt <sdyouatt@gmail.com>
2024-12-07 20:32:45 +0000

    for each line, split on the triple space and print

--- aoc2024/aocaml2024/bin/day1.ml
+++ aoc2024/aocaml2024/bin/day1.ml
@@ -1,1 +1,6 @@
-let () = Files.readfile "input.txt"
+let () =
+  let lines = Files.lines "inputs/day1.txt" in
+  List.iter (fun line -> match Strings.split_once "   " line with
+  | Some(x, y) -> Printf.printf "%s %s\n" x y
+  | None -> ()
+) lines
