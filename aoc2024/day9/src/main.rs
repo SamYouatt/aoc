a1b98c0952f1bef20e22693d5a9c5f58bf98b96c
SamYouatt <sdyouatt@gmail.com>
2024-12-09 11:12:34 +0000

    refactor part 1 using swap_remove

--- aoc2024/day9/src/main.rs
+++ aoc2024/day9/src/main.rs
@@ -11,42 +11,26 @@ fn part_1(input: &str) -> usize {
 
     for (i, char) in input.trim().chars().enumerate() {
         let digit = char.to_digit(10).unwrap();
-        for _ in 0..digit {
-            if i % 2 == 0 {
-                disk.push(Some(file_id));
-            } else {
-                disk.push(None);
-            }
-        }
-
         if i % 2 == 0 {
+            disk.append(&mut [Some(file_id)].repeat(digit as usize));
             file_id += 1;
+        } else {
+            disk.append(&mut [None].repeat(digit as usize));
         }
     }
 
-    let mut head_ptr = 0;
-    let mut sorted_marker = disk.len() - 1;
-    for tail_ptr in (0..disk.len()).rev() {
-        sorted_marker -= 1;
-        if disk[tail_ptr].is_none() {
-            continue;
-        }
-
-        while head_ptr < disk.len() && disk[head_ptr].is_some() {
-            head_ptr += 1;
-        }
-
-        if head_ptr == disk.len() {
-            continue;
-        }
-
-        if head_ptr >= sorted_marker {
+    for i in 0..disk.len() {
+        if i >= disk.len() {
             break;
         }
 
-        disk[head_ptr] = Some(disk[tail_ptr].unwrap());
-        disk[tail_ptr] = None;
-        head_ptr += 1;
+        while disk.last().unwrap().is_none() {
+            disk.remove(disk.len() - 1);
+        }
+
+        if disk[i].is_none() {
+            disk.swap_remove(i);
+        }
     }
 
     disk.iter()
