ae5cf6b0bb0c6ef32167e65825fafee1dfdc80fa
SamYouatt <sdyouatt@gmail.com>
2024-12-09 08:23:52 +0000

    day 9 part 2

--- aoc2024/day9/src/main.rs
+++ aoc2024/day9/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     let input = include_str!("input.txt");
 
     println!("Part 1: {}", part_1(input));
+    println!("Part 2: {}", part_2(input));
 }
 
 fn part_1(input: &str) -> usize {
@@ -51,4 +52,61 @@ fn part_1(input: &str) -> usize {
     disk.iter()
         .enumerate()
         .fold(0, |acc, (i, block)| acc + (block.unwrap_or(0) * i))
+}
+
+struct File {
+    id: usize,
+    start: usize,
+    len: usize,
+}
+
+struct Space {
+    start: usize,
+    len: usize,
+}
+
+fn part_2(input: &str) -> usize {
+    let map = input
+        .trim()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect::<Vec<_>>();
+
+    let mut files = Vec::new();
+    let mut file_id = 0;
+    let mut spaces = Vec::new();
+    let mut ptr = 0;
+
+    for (i, len) in map.iter().enumerate() {
+        if i % 2 == 0 {
+            files.push(File {
+                id: file_id,
+                start: ptr,
+                len: *len,
+            });
+            file_id += 1;
+        } else {
+            spaces.push(Space {
+                start: ptr,
+                len: *len,
+            })
+        }
+
+        ptr += len;
+    }
+
+    for file in files.iter_mut().rev() {
+        for space in spaces.iter_mut() {
+            if space.len >= file.len && space.start < file.start {
+                file.start = space.start;
+                space.start += file.len;
+                space.len -= file.len;
+                break;
+            }
+        }
+    }
+
+    files.iter().fold(0, |acc, f| {
+        acc + (f.start..(f.start + f.len)).fold(0, |i_acc, i| i_acc + (i * f.id))
+    })
 }
