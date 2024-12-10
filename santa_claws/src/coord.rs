20982bec491661f1e6e846fb93dbdffdc4bfed8b
SamYouatt <sdyouatt@gmail.com>
2024-12-10 20:51:00 +0000

    set up basic coord with macro coord!() which will cast usizes as well

--- santa_claws/src/coord.rs
+++ santa_claws/src/coord.rs
@@ -1,4 +1,41 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Coord {
     x: isize,
     y: isize,
+}
+
+/// Will cast x and y to isize, will panic if this is bad
+/// Example: coord!(3, 2)
+macro_rules! coord {
+    ($x:expr, $y:expr) => {
+        Coord {
+            x: $x as isize,
+            y: $y as isize,
+        }
+    };
+}
+
+impl Coord {
+    pub fn new(x: isize, y: isize) -> Self {
+        Self { x, y }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn coord_macro() {
+        let coord = coord!(3, 2);
+        assert_eq!(coord, Coord::new(3, 2));
+    }
+
+    #[test]
+    fn coord_macro_casts_usize() {
+        let x = 3_usize;
+        let y = 2_usize;
+        let coord = coord!(x, y);
+        assert_eq!(coord, Coord::new(3, 2));
+    }
 }
