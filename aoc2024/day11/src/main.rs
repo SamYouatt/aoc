afbc57c4db811ea754c99b44653b35e904c808c7
SamYouatt <sdyouatt@gmail.com>
2024-12-11 07:34:43 +0000

    refactor to recursive blink that'll be easier to cache

--- aoc2024/day11/src/main.rs
+++ aoc2024/day11/src/main.rs
@@ -2,44 +2,35 @@ fn main() {
     let input = include_str!("input.txt");
 
     println!("Part 1: {}", part_1(input));
+    //println!("Part 2: {}", part_2(input));
 }
 
 fn part_1(input: &str) -> usize {
-    let mut stones = input
+    input
         .trim()
         .split_whitespace()
         .map(|num| num.parse::<usize>().unwrap())
-        .collect::<Vec<_>>();
-
-    for _ in 0..25 {
-        stones = blink(stones);
-    }
-
-    stones.len()
+        .map(|num| blink(num, 0, 25))
+        .sum()
 }
 
-fn blink(stones: Vec<usize>) -> Vec<usize> {
-    let mut new = Vec::new();
-
-    for stone in stones {
-        if stone == 0 {
-            new.push(1);
-            continue;
-        }
-
-        let as_string = stone.to_string();
-        if as_string.len() % 2 == 0 {
-            let mid = (as_string.len() - 1) / 2;
-            let stone1 = as_string[..=mid].parse::<usize>().unwrap();
-            let stone2 = as_string[mid + 1..].parse::<usize>().unwrap();
-
-            new.push(stone1);
-            new.push(stone2);
-            continue;
-        }
-
-        new.push(stone * 2024);
+fn blink(stone: usize, blink_n: usize, limit: usize) -> usize {
+    if blink_n == limit {
+        return 1;
     }
 
-    new
+    if stone == 0 {
+        return blink(1, blink_n + 1, limit);
+    }
+
+    let as_string = stone.to_string();
+    if as_string.len() % 2 == 0 {
+        let mid = (as_string.len() - 1) / 2;
+        let stone1 = as_string[..=mid].parse::<usize>().unwrap();
+        let stone2 = as_string[mid + 1..].parse::<usize>().unwrap();
+
+        return blink(stone1, blink_n + 1, limit) + blink(stone2, blink_n + 1, limit);
+    }
+
+    return blink(stone * 2024, blink_n + 1, limit);
 }
