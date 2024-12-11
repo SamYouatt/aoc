619706f4c716b765660f427fafb97034438de2a6
SamYouatt <sdyouatt@gmail.com>
2024-12-11 07:43:44 +0000

    day 11 part 2
    adds caching for speeed

--- aoc2024/day11/src/main.rs
+++ aoc2024/day11/src/main.rs
@@ -1,8 +1,10 @@
+use std::collections::HashMap;
+
 fn main() {
     let input = include_str!("input.txt");
 
     println!("Part 1: {}", part_1(input));
-    //println!("Part 2: {}", part_2(input));
+    println!("Part 2: {}", part_2(input));
 }
 
 fn part_1(input: &str) -> usize {
@@ -10,27 +12,46 @@ fn part_1(input: &str) -> usize {
         .trim()
         .split_whitespace()
         .map(|num| num.parse::<usize>().unwrap())
-        .map(|num| blink(num, 0, 25))
+        .map(|num| blink(num, 0, 25, &mut HashMap::new()))
         .sum()
 }
 
-fn blink(stone: usize, blink_n: usize, limit: usize) -> usize {
+fn part_2(input: &str) -> usize {
+    input
+        .trim()
+        .split_whitespace()
+        .map(|num| num.parse::<usize>().unwrap())
+        .map(|num| blink(num, 0, 75, &mut HashMap::new()))
+        .sum()
+}
+
+fn blink(
+    stone: usize,
+    blink_n: usize,
+    limit: usize,
+    cache: &mut HashMap<(usize, usize), usize>,
+) -> usize {
     if blink_n == limit {
         return 1;
     }
 
-    if stone == 0 {
-        return blink(1, blink_n + 1, limit);
+    if let Some(cached) = cache.get(&(stone, blink_n)) {
+        return *cached;
     }
 
-    let as_string = stone.to_string();
-    if as_string.len() % 2 == 0 {
+    let result = if stone == 0 {
+        blink(1, blink_n + 1, limit, cache)
+    } else if stone.to_string().len() % 2 == 0 {
+        let as_string = stone.to_string();
         let mid = (as_string.len() - 1) / 2;
         let stone1 = as_string[..=mid].parse::<usize>().unwrap();
         let stone2 = as_string[mid + 1..].parse::<usize>().unwrap();
 
-        return blink(stone1, blink_n + 1, limit) + blink(stone2, blink_n + 1, limit);
-    }
+        blink(stone1, blink_n + 1, limit, cache) + blink(stone2, blink_n + 1, limit, cache)
+    } else {
+        blink(stone * 2024, blink_n + 1, limit, cache)
+    };
 
-    return blink(stone * 2024, blink_n + 1, limit);
+    cache.insert((stone, blink_n), result);
+    result
 }
