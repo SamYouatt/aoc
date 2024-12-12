e6eda25a47e1f74b6c603bb50fe9082b8748b728
SamYouatt <sdyouatt@gmail.com>
2024-12-12 09:21:14 +0000

    add direction deltas method to Direction

--- santa_claws/src/directions.rs
+++ santa_claws/src/directions.rs
@@ -1,6 +1,14 @@
+use crate::{coord::Delta, delta};
+
 pub enum Direction {
     Up,
     Down,
     Left,
     Right,
+}
+
+impl Direction {
+    pub fn deltas() -> Vec<Delta> {
+        [delta!(-1, 0), delta!(1, 0), delta!(0, -1), delta!(0, 1)].to_vec()
+    }
 }
