f46acb0002f8eda0a23819845289934b424f4bc1
SamYouatt <sdyouatt@gmail.com>
2024-12-14 10:04:43 +0000

    turn left for directions

--- santas_little_helpers/src/directions.rs
+++ santas_little_helpers/src/directions.rs
@@ -8,7 +8,16 @@ pub enum Direction {
 }
 
 impl Direction {
-    pub fn deltas() -> impl Iterator<Item=Delta> {
+    pub fn deltas() -> impl Iterator<Item = Delta> {
         [delta!(-1, 0), delta!(1, 0), delta!(0, -1), delta!(0, 1)].into_iter()
+    }
+
+    pub fn turn_right(self) -> Self {
+        match self {
+            Direction::Up => Direction::Right,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+            Direction::Right => Direction::Down,
+        }
     }
 }
