444fe8d61c832827966aea35d2faf374b73ecd50
SamYouatt <sdyouatt@gmail.com>
2024-12-14 10:04:56 +0000

    wip: updating day6 to use helpers

--- aoc2024/day6/src/main.rs
+++ aoc2024/day6/src/main.rs
@@ -1,9 +1,6 @@
-use std::{
-    collections::{HashMap, HashSet},
-    sync::atomic::AtomicUsize,
-};
+use std::collections::HashSet;
 
-use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
+use santas_little_helpers::coord::Coord;
 
 fn main() {
     let input = include_str!("input.txt");
@@ -14,94 +11,22 @@ fn main() {
 }
 
 #[derive(Clone)]
-enum Tile {
-    Obstacle,
-    Free,
-}
-
-#[derive(Hash, PartialEq, Eq, Debug, Clone, Copy)]
-struct Coord {
-    x: isize,
-    y: isize,
-}
-
-impl Coord {
-    fn new(x: isize, y: isize) -> Self {
-        Self { x, y }
-    }
-
-    fn apply_delta(self, delta: (isize, isize)) -> Self {
-        Self {
-            x: self.x + delta.0,
-            y: self.y + delta.1,
-        }
-    }
-
-    fn forwards(self, direction: Direction) -> Self {
-        self.apply_delta(direction.delta())
-    }
-}
-
-#[derive(Clone)]
 struct Map {
-    tiles: HashMap<Coord, Tile>,
-    width: isize,
-    height: isize,
+    tiles: Vec<Vec<u8>>,
+    width: usize,
+    height: usize,
 }
 
-impl Map {
-    fn new(tiles: HashMap<Coord, Tile>, width: isize, height: isize) -> Self {
-        Self {
-            tiles,
-            width,
-            height,
-        }
+fn facing_obstacle(&self, position: Coord, direction: Direction) -> bool {
+    let new_pos = direction.apply(&position);
+    if !self.in_bounds(new_pos) {
+        return false;
     }
 
-    fn in_bounds(&self, coord: Coord) -> bool {
-        coord.x >= 0 && coord.x < self.width && coord.y >= 0 && coord.y < self.height
-    }
-
-    fn facing_obstacle(&self, position: Coord, direction: Direction) -> bool {
-        let new_pos = position.apply_delta(direction.delta());
-
-        match self.tiles.get(&new_pos) {
-            Some(Tile::Obstacle) => true,
-            Some(Tile::Free) => false,
-            None => false,
-        }
-    }
-
-    fn set_tile(&mut self, position: Coord, tile: Tile) {
-        self.tiles.insert(position, tile);
-    }
-}
-
-#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
-}
-
-impl Direction {
-    fn turn_right(self) -> Self {
-        match self {
-            Direction::Up => Direction::Right,
-            Direction::Down => Direction::Left,
-            Direction::Left => Direction::Up,
-            Direction::Right => Direction::Down,
-        }
-    }
-
-    fn delta(&self) -> (isize, isize) {
-        match self {
-            Direction::Up => (0, -1),
-            Direction::Down => (0, 1),
-            Direction::Left => (-1, 0),
-            Direction::Right => (1, 0),
-        }
+    match self.tiles[new_pos.1 as usize][new_pos.0 as usize] {
+        b'#' => true,
+        b'.' => false,
+        _ => unreachable!(),
     }
 }
 
@@ -120,7 +45,7 @@ fn part_1(map: Map, mut guard: Guard) -> usize {
             guard.facing = guard.facing.turn_right();
         }
 
-        guard.pos = guard.pos.forwards(guard.facing);
+        guard.pos = forwards(&guard.pos, guard.facing);
 
         if !map.in_bounds(guard.pos) {
             break;
@@ -132,7 +57,7 @@ fn part_1(map: Map, mut guard: Guard) -> usize {
     visited.len()
 }
 
-fn part_2(map: Map, mut guard: Guard) -> usize {
+fn part_2(mut map: Map, mut guard: Guard) -> usize {
     let guard_start = guard.pos;
 
     let mut initially_visited = HashSet::new();
@@ -143,7 +68,7 @@ fn part_2(map: Map, mut guard: Guard) -> usize {
             guard.facing = guard.facing.turn_right();
         }
 
-        guard.pos = guard.pos.forwards(guard.facing);
+        guard.pos = forwards(&guard.pos, guard.facing);
 
         if !map.in_bounds(guard.pos) {
             break;
@@ -157,65 +82,99 @@ fn part_2(map: Map, mut guard: Guard) -> usize {
     guard.pos = guard_start;
     guard.facing = Direction::Up;
 
-    let valid_locations = AtomicUsize::new(0);
-    initially_visited.par_iter().for_each(|&obstacle_location| {
-        let mut this_guard = guard.clone();
-        let mut this_map = map.clone();
+    let mut valid_locations = 0;
+
+    for &obstacle_location in initially_visited.iter() {
         let mut visited = HashSet::<(Coord, Direction)>::new();
-        this_map.set_tile(obstacle_location, Tile::Obstacle);
+        map.set_tile(obstacle_location, b'#');
 
         loop {
-            while this_map.facing_obstacle(this_guard.pos, this_guard.facing) {
-                this_guard.facing = this_guard.facing.turn_right();
+            while map.facing_obstacle(guard.pos, guard.facing) {
+                guard.facing = guard.facing.turn_right();
             }
 
-            this_guard.pos = this_guard.pos.forwards(this_guard.facing);
+            guard.pos = forwards(&guard.pos, guard.facing);
 
-            if !this_map.in_bounds(this_guard.pos) {
+            if !map.in_bounds(guard.pos) {
                 break;
             }
 
-            if visited.contains(&(this_guard.pos, this_guard.facing)) {
-                valid_locations.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+            if visited.contains(&(guard.pos, guard.facing)) {
+                valid_locations += 1;
                 break;
             }
-            visited.insert((this_guard.pos, this_guard.facing));
+            visited.insert((guard.pos, guard.facing));
         }
-    });
 
-    valid_locations.load(std::sync::atomic::Ordering::Relaxed)
+        guard.pos = guard_start;
+        guard.facing = Direction::Up;
+        map.set_tile(obstacle_location, b'.');
+        visited.clear();
+    }
+
+    valid_locations
+
+    //let valid_locations = AtomicUsize::new(0);
+    //initially_visited.par_iter().for_each(|&obstacle_location| {
+    //    let mut this_guard = guard.clone();
+    //    let mut this_map = map.clone();
+    //    let mut visited = HashSet::<(Coord, Direction)>::new();
+    //    this_map.set_tile(obstacle_location, b'#');
+    //
+    //    loop {
+    //        while this_map.facing_obstacle(this_guard.pos, this_guard.facing) {
+    //            this_guard.facing = this_guard.facing.turn_right();
+    //        }
+    //
+    //        this_guard.pos = forwards(&this_guard.pos, this_guard.facing);
+    //
+    //        if !this_map.in_bounds(this_guard.pos) {
+    //            break;
+    //        }
+    //
+    //        if visited.contains(&(this_guard.pos, this_guard.facing)) {
+    //            valid_locations.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+    //            break;
+    //        }
+    //        visited.insert((this_guard.pos, this_guard.facing));
+    //    }
+    //});
+
+    //valid_locations.load(std::sync::atomic::Ordering::Relaxed)
 }
 
 fn parse(input: &str) -> (Map, Guard) {
-    let mut tiles = HashMap::new();
     let mut guard = Guard {
-        pos: Coord::new(0, 0),
+        pos: (0, 0),
         facing: Direction::Up,
     };
 
     let width = input.lines().nth(1).unwrap().bytes().len();
     let height = input.lines().count();
 
+    let mut tiles = Vec::new();
     for (y, line) in input.lines().enumerate() {
+        let mut row = Vec::new();
         for (x, tile) in line.bytes().enumerate() {
-            let coord = Coord::new(x as isize, y as isize);
+            let coord = (x, y);
             match tile {
                 b'#' => {
-                    tiles.insert(coord, Tile::Obstacle);
+                    row.push(b'#');
                 }
                 b'.' => {
-                    tiles.insert(coord, Tile::Free);
+                    row.push(b'.');
                 }
                 b'^' => {
                     guard.pos = coord;
-                    tiles.insert(coord, Tile::Free);
+                    row.push(b'.');
                 }
                 _ => panic!("bad tile"),
             }
         }
+        tiles.push(row);
     }
 
-    let map = Map::new(tiles, width as isize, height as isize);
+    let map = Map::new(tiles, width as usize, height as usize);
 
     (map, guard)
 }
