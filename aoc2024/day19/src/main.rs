3060e2cdc7e027f85f3237b11b7e85b471a4dc0a
SamYouatt <sdyouatt@gmail.com>
2024-12-19 09:01:19 +0000

    day 19 part 2

--- aoc2024/day19/src/main.rs
+++ aoc2024/day19/src/main.rs
@@ -1,10 +1,13 @@
+use std::collections::HashMap;
+
 fn main() {
     let input = include_str!("input.txt");
 
     println!("Part 1: {}", part_1(input));
+    println!("Part 2: {}", part_2(input));
 }
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, Hash)]
 enum Towel {
     Red,
     White,
@@ -39,21 +42,52 @@ fn part_1(input: &str) -> usize {
         .map(|d| d.trim().bytes().map(Towel::from_byte).collect::<Vec<_>>())
         .collect::<Vec<_>>();
 
-    designs.iter().filter(|d| possible(&d, &towels)).count()
+    let mut cache = HashMap::new();
+    designs
+        .iter()
+        .filter(|d| all_possible(&d, &towels, &mut cache) > 0)
+        .count()
 }
 
-fn possible(design: &[Towel], towels: &Vec<Vec<Towel>>) -> bool {
+fn part_2(input: &str) -> usize {
+    let (towels, designs) = input.split_once("\n\n").unwrap();
+
+    let towels = towels
+        .split(", ")
+        .map(|t| t.bytes().map(Towel::from_byte).collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let designs = designs
+        .lines()
+        .map(|d| d.trim().bytes().map(Towel::from_byte).collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let mut cache = HashMap::new();
+    designs
+        .iter()
+        .fold(0, |acc, d| acc + all_possible(&d, &towels, &mut cache))
+}
+
+fn all_possible<'a>(
+    design: &'a [Towel],
+    towels: &Vec<Vec<Towel>>,
+    cache: &mut HashMap<&'a [Towel], usize>,
+) -> usize {
     if design.is_empty() {
-        return true;
+        return 1;
     }
 
+    if let Some(n) = cache.get(&design) {
+        return *n;
+    }
+
+    let mut ways = 0;
     for t in towels {
         if design.starts_with(t) {
-            if possible(&design[t.len()..], towels) {
-                return true;
-            }
+            ways += all_possible(&design[t.len()..], towels, cache);
         }
     }
 
-    false
+    cache.insert(&design, ways);
+    ways
 }
