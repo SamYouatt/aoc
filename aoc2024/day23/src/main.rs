4ea1ca7efa7945379db3f8fab2dc6482b3ef52c5
SamYouatt <sdyouatt@gmail.com>
2024-12-23 07:55:33 +0000

    day 23 part 2

--- aoc2024/day23/src/main.rs
+++ aoc2024/day23/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     let input = include_str!("input.txt");
 
     println!("Part 1: {}", part_1(input));
+    println!("Part 2: {}", part_2(input));
 }
 
 fn part_1(input: &str) -> usize {
@@ -38,4 +39,37 @@ fn part_1(input: &str) -> usize {
     }
 
     triangles.len()
+}
+
+fn part_2(input: &str) -> String {
+    let mut graph: HashMap<&str, HashSet<&str>> = HashMap::new();
+
+    for line in input.lines() {
+        let (x, y) = line.split_once('-').unwrap();
+
+        graph.entry(x).or_default().insert(y);
+        graph.entry(y).or_default().insert(x);
+    }
+
+    let mut connected_regions = HashSet::new();
+    for (node, conns) in graph.iter() {
+        let mut region = vec![node];
+
+        for neighbour in conns {
+            let n_conns = &graph[neighbour];
+            if region.iter().all(|r| n_conns.contains(*r)) {
+                region.push(neighbour);
+            }
+        }
+
+        region.sort();
+        connected_regions.insert(region);
+    }
+
+    connected_regions
+        .iter()
+        .max_by(|x, y| x.len().cmp(&y.len()))
+        .unwrap()
+        .into_iter()
+        .join(",")
 }
