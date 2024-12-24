20ec91061857e0bbc2ec5fddce2fceacbd9dea76
SamYouatt <sdyouatt@gmail.com>
2024-12-24 09:13:22 +0000

    small rejig

--- aoc2024/day24/src/main.rs
+++ aoc2024/day24/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     let input = include_str!("input.txt");
 
     println!("Part 1: {}", part_1(input));
+    println!("Part 2: {}", part_2(input));
 }
 
 #[derive(Debug)]
@@ -34,6 +35,71 @@ impl Default for Gate<'_> {
 }
 
 fn part_1(input: &str) -> usize {
+    let mut circuit = parse_circuit(input);
+
+    run_circuit(&mut circuit);
+
+    let mut zs = circuit
+        .keys()
+        .filter(|k| k.starts_with('z'))
+        .collect::<Vec<_>>();
+    zs.sort();
+    let bits = zs.iter().rev().fold(String::new(), |mut b, z| {
+        let bit = match circuit[**z].value {
+            Some(true) => '1',
+            Some(false) => '0',
+            None => panic!(),
+        };
+        b.push(bit);
+        b
+    });
+
+    usize::from_str_radix(&bits, 2).unwrap()
+}
+
+fn part_2(input: &str) -> usize {
+    todo!()
+}
+
+fn run_circuit(circuit: &mut HashMap<&str, Gate<'_>>) {
+    let mut queue = VecDeque::new();
+    circuit.keys().for_each(|k| queue.push_back(*k));
+
+    while let Some(gate_key) = queue.pop_front() {
+        let gate = circuit.get(gate_key).unwrap();
+        if gate.value.is_some() {
+            continue;
+        }
+
+        let dep_a = circuit.get(gate.a.unwrap()).unwrap();
+        let dep_b = &circuit[gate.b.unwrap()];
+
+        if dep_a.value.is_none() || dep_b.value.is_none() {
+            queue.push_back(gate_key);
+            continue;
+        }
+
+        let val_a = dep_a.value.unwrap();
+        let val_b = dep_b.value.unwrap();
+
+        let gate = circuit.get_mut(gate_key).unwrap();
+        gate.value = gate_logic(gate, val_a, val_b);
+    }
+}
+
+fn gate_logic(gate: &Gate<'_>, val_a: bool, val_b: bool) -> Option<bool> {
+    match (&gate.gate_type, val_a, val_b) {
+        (Type::AND, true, true) => Some(true),
+        (Type::AND, _, _) => Some(false),
+        (Type::OR, false, false) => Some(false),
+        (Type::OR, _, _) => Some(true),
+        (Type::XOR, true, false) | (Type::XOR, false, true) => Some(true),
+        (Type::XOR, _, _) => Some(false),
+        (Type::NONE, _, _) => None,
+    }
+}
+
+fn parse_circuit(input: &str) -> HashMap<&str, Gate<'_>> {
     let (wires, raw_circuit) = input.split_once("\n\n").unwrap();
 
     let mut circuit = HashMap::new();
@@ -72,56 +138,5 @@ fn part_1(input: &str) -> usize {
         circuit.get_mut(gate).unwrap().value = Some(signal);
     }
 
-    let mut queue = VecDeque::new();
-    circuit.keys().for_each(|k| queue.push_back(*k));
-
-    while let Some(gate_key) = queue.pop_front() {
-        let gate = circuit.get(gate_key).unwrap();
-        if gate.value.is_some() {
-            continue;
-        }
-
-        let dep_a = circuit.get(gate.a.unwrap()).unwrap();
-        let dep_b = &circuit[gate.b.unwrap()];
-
-        if dep_a.value.is_none() || dep_b.value.is_none() {
-            queue.push_back(gate_key);
-            continue;
-        }
-
-        let val_a = dep_a.value.unwrap();
-        let val_b = dep_b.value.unwrap();
-
-        let gate = circuit.get_mut(gate_key).unwrap();
-        gate.value = gate_logic(gate, val_a, val_b);
-    }
-
-    let mut zs = circuit
-        .keys()
-        .filter(|k| k.starts_with('z'))
-        .collect::<Vec<_>>();
-    zs.sort();
-    let bits = zs.iter().rev().fold(String::new(), |mut b, z| {
-        let bit = match circuit[**z].value {
-            Some(true) => '1',
-            Some(false) => '0',
-            None => panic!(),
-        };
-        b.push(bit);
-        b
-    });
-
-    usize::from_str_radix(&bits, 2).unwrap()
-}
-
-fn gate_logic(gate: &Gate<'_>, val_a: bool, val_b: bool) -> Option<bool> {
-    match (&gate.gate_type, val_a, val_b) {
-        (Type::AND, true, true) => Some(true),
-        (Type::AND, _, _) => Some(false),
-        (Type::OR, false, false) => Some(false),
-        (Type::OR, _, _) => Some(true),
-        (Type::XOR, true, false) | (Type::XOR, false, true) => Some(true),
-        (Type::XOR, _, _) => Some(false),
-        (Type::NONE, _, _) => None,
-    }
+    circuit
 }
