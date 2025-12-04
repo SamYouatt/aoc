e7f49b2bf722a12c337789008de5d28acdaad16f
Sam Youatt <samyouatt@Sams-MacBook-Pro.local>
2025-12-05 08:58:02 +0900

    flesh out more of the grid helper

--- elixir/klaus/lib/klaus/grid.ex
+++ elixir/klaus/lib/klaus/grid.ex
@@ -49,4 +49,54 @@ defmodule Klaus.Grid do
   def update(%__MODULE__{data: data} = grid, coord, default, fun) do
     %{grid | data: Map.update(data, coord, default, fun)}
   end
+
+  @spec in_bounds?(t(), coord()) :: boolean()
+  def in_bounds?(%__MODULE__{width: width, height: height}, {x, y}) do
+    x >= 0 and x < width and y >= 0 and y < height
+  end
+
+  @doc "Get all the values in a single row at y"
+  @spec row(t(), integer()) :: [any()]
+  def row(%__MODULE__{data: data, width: width}, y) do
+    for x <- 0..(width - 1), do: Map.get(data, {x, y})
+  end
+
+  @doc "Get all row as list of lists"
+  @spec rows(t()) :: [[any()]]
+  def rows(%__MODULE__{height: height} = grid) do
+    for y <- 0..(height - 1), do: row(grid, y)
+  end
+
+  @neighbours [{-1, 0}, {1, 0}, {0, -1}, {0, 1}]
+  @all_neighbours for dy <- -1..1, dx <- -1..1, {dx, dy} != {0, 0}, do: {dx, dy}
+
+  @doc "Get all the values of in bound neighbours. Default is direct neighbours. :any can be specified to include diagonals"
+  @spec neighbours(t(), coord(), :direct | :all) :: [any()]
+  def neighbours(grid, coord, mode \\ :direct)
+
+  def neighbours(%__MODULE__{} = grid, {x, y}, :direct) do
+    @neighbours
+    |> Enum.map(fn {dx, dy} -> {x + dx, y + dy} end)
+    |> Enum.filter(&in_bounds?(grid, &1))
+    |> Enum.map(fn c -> get(grid, c) end)
+  end
+
+  def neighbours(%__MODULE__{} = grid, {x, y}, :all) do
+    @all_neighbours
+    |> Enum.map(fn {dx, dy} -> {x + dx, y + dy} end)
+    |> Enum.filter(&in_bounds?(grid, &1))
+    |> Enum.map(fn c -> get(grid, c) end)
+  end
+
+  @spec to_string(t(), (any() -> String.t())) :: String.t()
+  def to_string(%__MODULE__{} = grid, formatter \\ &Kernel.to_string/1) do
+    grid
+    |> rows()
+    |> Enum.map(fn row -> Enum.map_join(row, "", formatter) end)
+    |> Enum.join("\n")
+  end
+
+  defimpl String.Chars do
+    def to_string(grid), do: Klaus.Grid.to_string(grid)
+  end
 end
