529c7f3c80f2cedace965fa0caf7c4aca3a6b308
Sam Youatt <samyouatt@Sams-MacBook-Pro.local>
2025-12-08 12:49:03 +0000

    day 8 refactor

--- aoc2025/elixir2025/lib/day8.ex
+++ aoc2025/elixir2025/lib/day8.ex
@@ -1,44 +1,10 @@
 defmodule Day8 do
   def part1() do
-    sorted_pairs =
-      Klaus.Input.parse_lines(8, &parse_box/1)
-      |> then(fn boxes ->
-        for a <- boxes,
-            b <- boxes,
-            a < b,
-            do: {a, b, distance(a, b)}
-      end)
-      |> Enum.sort_by(&elem(&1, 2))
-      |> Enum.map(fn {a, b, _} -> {a, b} end)
+    {initial_circuits, pairs} = setup()
 
     0..999
-    |> Enum.reduce({[], sorted_pairs}, fn _, {circuits, [{a, b} | rem_pairs]} ->
-      new_circuits =
-        case {containing(circuits, a), containing(circuits, b)} do
-          {nil, nil} ->
-            [MapSet.new([a, b]) | circuits]
-
-          {found_a, nil} ->
-            circuits
-            |> List.delete(found_a)
-            |> then(fn rest -> [MapSet.put(found_a, b) | rest] end)
-
-          {nil, found_b} ->
-            circuits
-            |> List.delete(found_b)
-            |> then(fn rest -> [MapSet.put(found_b, a) | rest] end)
-
-          {found_a, found_b} when found_a == found_b ->
-            circuits
-
-          {found_a, found_b} ->
-            circuits
-            |> List.delete(found_a)
-            |> List.delete(found_b)
-            |> then(fn rest -> [MapSet.union(found_a, found_b) | rest] end)
-        end
-
-      {new_circuits, rem_pairs}
+    |> Enum.reduce({initial_circuits, pairs}, fn _, {circuits, [{a, b} | rem_pairs]} ->
+      {merge_circuits(circuits, a, b), rem_pairs}
     end)
     |> then(&elem(&1, 0))
     |> Enum.sort_by(&MapSet.size/1, :desc)
@@ -48,9 +14,36 @@ defmodule Day8 do
   end
 
   def part2() do
+    {initial_circuits, pairs} = setup()
+    collapse_circuits(initial_circuits, pairs)
+  end
+
+  defp collapse_circuits(circuits, [{a, b} | rem]) do
+    new_circuits = merge_circuits(circuits, a, b)
+
+    if length(new_circuits) == 1,
+      do: elem(a, 0) * elem(b, 0),
+      else: collapse_circuits(new_circuits, rem)
+  end
+
+  defp merge_circuits(circuits, a, b) do
+    found_a = containing(circuits, a)
+    found_b = containing(circuits, b)
+
+    if found_a == found_b do
+      circuits
+    else
+      circuits
+      |> List.delete(found_a)
+      |> List.delete(found_b)
+      |> then(fn rest -> [MapSet.union(found_a, found_b) | rest] end)
+    end
+  end
+
+  defp setup do
     boxes = Klaus.Input.parse_lines(8, &parse_box/1)
 
-    sorted_pairs =
+    pairs =
       boxes
       |> then(fn boxes ->
         for a <- boxes,
@@ -61,43 +54,15 @@ defmodule Day8 do
       |> Enum.sort_by(&elem(&1, 2))
       |> Enum.map(fn {a, b, _} -> {a, b} end)
 
-    initial_sets = Enum.map(boxes, &MapSet.new([&1]))
+    initial_circuits = Enum.map(boxes, &MapSet.new([&1]))
 
-    collapse_circuits(initial_sets, sorted_pairs)
+    {initial_circuits, pairs}
   end
 
-  defp collapse_circuits(circuits, [{a, b} | rem]) do
-    new_circuits =
-      case {containing(circuits, a), containing(circuits, b)} do
-        {nil, nil} ->
-          [MapSet.new([a, b]) | circuits]
+  defp distance({x1, y1, z1}, {x2, y2, z2}),
+    do: :math.sqrt(:math.pow(x2 - x1, 2) + :math.pow(y2 - y1, 2) + :math.pow(z2 - z1, 2))
 
-        {found_a, nil} ->
-          circuits
-          |> List.delete(found_a)
-          |> then(fn rest -> [MapSet.put(found_a, b) | rest] end)
-
-        {nil, found_b} ->
-          circuits
-          |> List.delete(found_b)
-          |> then(fn rest -> [MapSet.put(found_b, a) | rest] end)
-
-        {found_a, found_b} when found_a == found_b ->
-          circuits
-
-        {found_a, found_b} ->
-          circuits
-          |> List.delete(found_a)
-          |> List.delete(found_b)
-          |> then(fn rest -> [MapSet.union(found_a, found_b) | rest] end)
-      end
-
-    if length(new_circuits) == 1 do
-      elem(a, 0) * elem(b, 0)
-    else
-      collapse_circuits(new_circuits, rem)
-    end
-  end
+  defp containing(circuits, box), do: Enum.find(circuits, nil, &MapSet.member?(&1, box))
 
   defp parse_box(line) do
     line
@@ -105,9 +70,4 @@ defmodule Day8 do
     |> Enum.map(&String.to_integer/1)
     |> List.to_tuple()
   end
-
-  defp distance({x1, y1, z1}, {x2, y2, z2}),
-    do: :math.sqrt(:math.pow(x2 - x1, 2) + :math.pow(y2 - y1, 2) + :math.pow(z2 - z1, 2))
-
-  defp containing(circuits, box), do: Enum.find(circuits, nil, &MapSet.member?(&1, box))
 end
