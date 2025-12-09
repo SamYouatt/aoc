d534485ea8eb3d11b099e427f785a042ca433ad9
Sam Youatt <samyouatt@Sams-MacBook-Pro.local>
2025-12-09 20:21:41 +0000

    massively simplify day 9
    instead of checking if the area is inside the complex polygon, just
    check if any of the polygons edges intersect the rectangle

--- aoc2025/elixir2025/lib/day9.ex
+++ aoc2025/elixir2025/lib/day9.ex
@@ -10,62 +10,28 @@ defmodule Day9 do
   def part2() do
     red_tiles = Klaus.Input.parse_lines(9, &parse_tile/1)
 
+    green_edges =
+      Enum.chunk_every(red_tiles, 2, 1, [hd(red_tiles)])
+      |> Enum.map(fn [a, b] -> normalise(a, b) end)
+
     for a <- red_tiles, b <- red_tiles, a < b do
-      {a, b, area(a, b)}
+      {norm_a, norm_b} = normalise(a, b)
+      {norm_a, norm_b, area(a, b)}
     end
     |> Enum.sort_by(fn {_, _, area} -> area end, :desc)
     |> Enum.find_value(fn {a, b, area} ->
-      if rectangle_in_polygon(a, b, red_tiles), do: area
+      if !rect_intersected?(a, b, green_edges), do: area
     end)
   end
 
-  def rectangle_in_polygon({x1, y1}, {x2, y2}, polygon_vertices) do
-    poly_edges = Enum.chunk_every(polygon_vertices, 2, 1, [hd(polygon_vertices)])
-    corners = [{x1, y1}, {x1, y2}, {x2, y1}, {x2, y2}]
-
-    rect_edges = [
-      {{x1, y1}, {x2, y1}},
-      {{x2, y1}, {x2, y2}},
-      {{x2, y2}, {x1, y2}},
-      {{x1, y2}, {x1, y1}}
-    ]
-
-    Enum.all?(corners, &point_inside(&1, poly_edges)) and
-      Enum.all?(rect_edges, fn edge ->
-        Enum.all?(poly_edges, fn [p1, p2] -> !lines_intersect(edge, {p1, p2}) end)
-      end)
-  end
-
-  # This is a nasty implementation of the approach to counting boundaries as walking out the shape. If we cross odd boundaries we are inside, even we are outside.
-  defp point_inside({x, y} = point, edges) do
-    Enum.reduce_while(edges, false, fn [{x1, y1} = edge1, {x2, y2} = edge2], inside ->
-      cond do
-        # points on lines are considered inside
-        point_on_line(point, edge1, edge2) -> {:halt, true}
-        # if the edge is horizonal then skip it, ray casting only outwards to the right so this is wasted computation
-        y1 > y == y2 > y or y1 == y2 -> {:cont, inside}
-        # check if the edge is to the right of the point in question, i.e. it would be hit by a raycast to the right
-        x < (x2 - x1) * (y - y1) / (y2 - y1) + x1 -> {:cont, not inside}
-        true -> {:cont, inside}
-      end
+  defp rect_intersected?({x1, y1}, {x2, y2}, edges) do
+    Enum.any?(edges, fn {{p, q}, {r, s}} ->
+      p < x2 and q < y2 and r > x1 and s > y1
     end)
   end
 
-  defp point_on_line({x, y} = point, {ex1, ey1} = edge1, {ex2, ey2} = edge2) do
-    if cross_prod(edge1, edge2, point) != 0,
-      do: false,
-      else: x >= min(ex1, ex2) && x <= max(ex1, ex2) && y >= min(ey1, ey2) && y <= max(ey1, ey2)
-  end
-
-  defp lines_intersect({a1, a2}, {b1, b2}) do
-    b_straddles_a = cross_prod(a1, a2, b1) * cross_prod(a1, a2, b2) < 0
-    a_straddles_b = cross_prod(b1, b2, a1) * cross_prod(b1, b2, a2) < 0
-    b_straddles_a and a_straddles_b
-  end
-
-  # Will return < 0 when x is below/left the line, and >0 when above/right, and 0 when on the line
-  defp cross_prod({lx1, ly1}, {lx2, ly2}, {x, y}),
-    do: (lx2 - lx1) * (y - ly1) - (ly2 - ly1) * (x - lx1)
+  # Given any two opposite corners, work out the top right and bottom left of that rectangle
+  defp normalise({x1, y1}, {x2, y2}), do: {{min(x1, x2), min(y1, y2)}, {max(x1, x2), max(y1, y2)}}
 
   defp area({x1, y1}, {x2, y2}), do: (abs(x2 - x1) + 1) * (abs(y2 - y1) + 1)
 
