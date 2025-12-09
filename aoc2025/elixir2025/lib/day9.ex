49c6e33a767055922813ae89d855584be7ea2e22
Sam Youatt <samyouatt@Sams-MacBook-Pro.local>
2025-12-09 13:02:45 +0000

    day 9 refactoring

--- aoc2025/elixir2025/lib/day9.ex
+++ aoc2025/elixir2025/lib/day9.ex
@@ -1,13 +1,9 @@
 defmodule Day9 do
   def part1() do
-    tiles =
-      Klaus.Input.parse_lines(9, &parse_tile/1)
-
-    for a <- tiles,
-        b <- tiles,
-        a < b do
-      area(a, b)
-    end
+    Klaus.Input.parse_lines(9, &parse_tile/1)
+    |> then(fn tiles ->
+      for a <- tiles, b <- tiles, a < b, do: area(a, b)
+    end)
     |> Enum.max()
   end
 
@@ -26,48 +22,33 @@ defmodule Day9 do
   end
 
   def rectangle_in_polygon({x1, y1}, {x2, y2}, polygon_vertices) do
-    corners = [{x1, y1}, {x1, y2}, {x2, y1}, {x2, y2}]
     poly_edges = Enum.chunk_every(polygon_vertices, 2, 1, [hd(polygon_vertices)])
+    corners = [{x1, y1}, {x1, y2}, {x2, y1}, {x2, y2}]
 
-    corners_good = Enum.all?(corners, &point_inside(&1, poly_edges))
+    rect_edges = [
+      {{x1, y1}, {x2, y1}},
+      {{x2, y1}, {x2, y2}},
+      {{x2, y2}, {x1, y2}},
+      {{x1, y2}, {x1, y1}}
+    ]
 
-    if !corners_good do
-      false
-    else
-      rect_edges = [
-        {{x1, y1}, {x2, y1}},
-        {{x2, y1}, {x2, y2}},
-        {{x2, y2}, {x1, y2}},
-        {{x1, y2}, {x1, y1}}
-      ]
-
-      edges_good =
-        Enum.all?(rect_edges, fn edge ->
-          Enum.all?(poly_edges, fn [p1, p2] -> !lines_intersect(edge, {p1, p2}) end)
-        end)
-
-      edges_good
-    end
+    Enum.all?(corners, &point_inside(&1, poly_edges)) and
+      Enum.all?(rect_edges, fn edge ->
+        Enum.all?(poly_edges, fn [p1, p2] -> !lines_intersect(edge, {p1, p2}) end)
+      end)
   end
 
   # This is a nasty implementation of the approach to counting boundaries as walking out the shape. If we cross odd boundaries we are inside, even we are outside.
   defp point_inside({x, y} = point, edges) do
     Enum.reduce_while(edges, false, fn [{x1, y1} = edge1, {x2, y2} = edge2], inside ->
-      # points on lines are considered inside
-      if point_on_line(point, edge1, edge2) do
-        {:halt, true}
-      else
+      cond do
+        # points on lines are considered inside
+        point_on_line(point, edge1, edge2) -> {:halt, true}
         # if the edge is horizonal then skip it, ray casting only outwards to the right so this is wasted computation
-        if y1 > y == y2 > y or y1 == y2 do
-          {:cont, inside}
-        else
-          # do the actual rightwards ray casting
-          if x < (x2 - x1) * (y - y1) / (y2 - y1) + x1 do
-            {:cont, not inside}
-          else
-            {:cont, inside}
-          end
-        end
+        y1 > y == y2 > y or y1 == y2 -> {:cont, inside}
+        # do the actual rightwards ray casting
+        x < (x2 - x1) * (y - y1) / (y2 - y1) + x1 -> {:cont, not inside}
+        true -> {:cont, inside}
       end
     end)
   end
