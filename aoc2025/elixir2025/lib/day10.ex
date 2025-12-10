0d1155397814c5f36dcb840699dfb8c0cf8247d0
Sam Youatt <samyouatt@Sams-MacBook-Pro.local>
2025-12-10 06:06:16 +0000

    day 10 part 2

--- aoc2025/elixir2025/lib/day10.ex
+++ aoc2025/elixir2025/lib/day10.ex
@@ -9,6 +9,16 @@ defmodule Day10 do
     |> Enum.sum()
   end
 
+  def part2() do
+    Klaus.Input.parse_lines(10, &parse_line/1)
+    |> Enum.map(fn {_, buttons, goal} ->
+      initial = List.duplicate(0, length(goal))
+      visited = MapSet.new([initial])
+      search2([{initial, 0}], buttons, goal, visited)
+    end)
+    |> Enum.sum()
+  end
+
   defp search([{indicator, depth} | _rest], _buttons, goal, _visited) when indicator == goal do
     depth
   end
@@ -33,6 +43,37 @@ defmodule Day10 do
     end)
   end
 
+  defp search2([{indicator, depth} | _rest], _buttons, goal, _visited) when indicator == goal do
+    depth
+  end
+
+  defp search2([], _buttons, _goal, _visited), do: raise("Something wrong we ran out of states")
+
+  defp search2([{indicator, depth} | rest], buttons, goal, visited) do
+    next_states =
+      buttons
+      |> Enum.map(fn mask -> {increment(indicator, mask), depth + 1} end)
+      |> Enum.reject(fn {indicators, _} ->
+        MapSet.member?(visited, indicators) or exceeds(goal, indicators)
+      end)
+
+    new_visited =
+      Enum.reduce(next_states, visited, fn {state, _}, acc -> MapSet.put(acc, state) end)
+
+    search2(rest ++ next_states, buttons, goal, new_visited)
+  end
+
+  defp increment(counters, mask) do
+    Enum.reduce(mask, counters, fn index, acc ->
+      List.update_at(acc, index, &(&1 + 1))
+    end)
+  end
+
+  defp exceeds(goal, counters) do
+    Enum.zip(counters, goal)
+    |> Enum.any?(fn {a, b} -> a > b end)
+  end
+
   defp parse_line(line) do
     regex = ~r/\[([.#]+)\]\s+((?:\([0-9,]+\)\s*)+)\{([0-9,]+)\}/
     [_, goal, buttons, joltage] = Regex.run(regex, line)
@@ -53,6 +94,11 @@ defmodule Day10 do
         |> Enum.map(&String.to_integer/1)
       end)
 
-    {parsed_goal, parsed_numbers, joltage}
+    parsed_joltages =
+      joltage
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+
+    {parsed_goal, parsed_numbers, parsed_joltages}
   end
 end
