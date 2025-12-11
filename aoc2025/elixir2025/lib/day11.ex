b601df17a49408cba80bcbb1cb684f71d5a15108
Sam Youatt <samyouatt@Sams-MacBook-Pro.local>
2025-12-11 06:27:35 +0000

    day 11 part 2

--- aoc2025/elixir2025/lib/day11.ex
+++ aoc2025/elixir2025/lib/day11.ex
@@ -1,12 +1,19 @@
 defmodule Day11 do
   def part1() do
-    graph =
-      Klaus.Input.parse_lines(11, &parse_line/1)
-      |> Enum.reduce(%{}, fn {node, edges}, graph ->
-        Map.put(graph, node, edges)
-      end)
+    Klaus.Input.parse_lines(11, &parse_line/1)
+    |> Enum.reduce(%{}, fn {node, edges}, graph ->
+      Map.put(graph, node, edges)
+    end)
+    |> traverse("you", MapSet.new())
+  end
 
-    traverse(graph, "you", MapSet.new())
+  def part2() do
+    Klaus.Input.parse_lines(11, &parse_line/1)
+    |> Enum.reduce(%{}, fn {node, edges}, graph ->
+      Map.put(graph, node, edges)
+    end)
+    |> dag_traverse("svr", false, false, %{})
+    |> then(&elem(&1, 0))
   end
 
   defp traverse(_graph, "out", _visited), do: 1
@@ -20,6 +27,37 @@ defmodule Day11 do
     |> Enum.reduce(0, fn n, count ->
       count + traverse(graph, n, new_visited)
     end)
+  end
+
+  defp dag_traverse(_graph, "out", seen_dac, seen_fft, cache) do
+    result = if seen_dac and seen_fft, do: 1, else: 0
+    {result, cache}
+  end
+
+  defp dag_traverse(graph, current, seen_dac, seen_fft, cache) do
+    key = {current, seen_dac, seen_fft}
+
+    case Map.get(cache, key) do
+      nil ->
+        new_seen_dac = seen_dac or current == "dac"
+        new_seen_fft = seen_fft or current == "fft"
+        next = Map.get(graph, current)
+
+        {result, new_cache} =
+          next
+          |> Enum.reduce({0, cache}, fn n, {count, acc_cache} ->
+            {n_result, updated_cache} =
+              dag_traverse(graph, n, new_seen_dac, new_seen_fft, acc_cache)
+
+            {count + n_result, updated_cache}
+          end)
+
+        final_cache = Map.put(new_cache, key, result)
+        {result, final_cache}
+
+      cached_result ->
+        {cached_result, cache}
+    end
   end
 
   defp parse_line(line) do
